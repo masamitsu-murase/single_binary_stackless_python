b77c9f6814b4399241b6b53f21ab810fa38d05ad
Kristjan Valur Jonsson <sweskman@gmail.com>
2013-11-11 14:50:58 +0000

    Unittest for thread pickling

--- Stackless/unittests/test_thread.py
+++ Stackless/unittests/test_thread.py
@@ -10,6 +10,7 @@ import contextlib
 from support import StacklessTestCase, AsTaskletTestCase
 try:
     import threading
+    import thread
     withThreads = True
 except:
     withThreads = False
@@ -105,6 +106,71 @@ class TestRemoteSchedule(AsTaskletTestCase):
         finally:
             thread.join(2)
         self.assertEqual(self.events, list(range(3)))
+
+@unittest.skipUnless(withThreads, "requires thread support")
+class TestRebindCrash(unittest.TestCase):
+    """A crash from Anselm Kruis, occurring when transferring tasklet to a thread"""
+    def create_remote_tasklet(self):
+        result = []
+        e1 = threading.Event()
+        e2 = threading.Event()
+        def taskletfunc():
+            result.append(stackless.getcurrent())
+            stackless.schedule_remove(retval=None)
+        def threadfunc():
+            t = stackless.tasklet(taskletfunc)()
+            t.run()
+            e1.set()
+            e2.wait() #wait until we can die
+        t = threading.Thread(target=threadfunc)
+        t.start()
+        e1.wait()
+
+        # callable to end the thread
+        def end():
+            e2.set()
+            t.join()
+        return end, result[0]
+
+    def to_current_thread(self, task):
+        """
+        Get a tasklet for the current thread.
+
+        If the tasklet already belongs to the current thread, this
+        method returns the tasklet unmodified.
+
+        Otherwise, this method tries to
+        unbind the tasklet and returns a newly created tasklet. If
+        unbinding fails, the method raises :exc:`RuntimeError`.
+        """
+        if task.thread_id == thread.get_ident():
+            return task
+        reducedTask = task.__reduce__()
+        # raise RuntimeError, if task is alive but not paused
+        task.bind(None)
+
+        if False:  # python will crash if set to False
+            frameList = reducedTask[2][3]
+            for i in range(len(frameList)):
+                frame = frameList[i]
+                if isinstance(frame, stackless.cframe):
+                    reducedFrame = frame.__reduce__()
+                    newFrame = reducedFrame[0](*reducedFrame[1])
+                    newFrame.__setstate__(reducedFrame[2])
+                    frameList[i] = newFrame
+        # rebind the task
+        task = reducedTask[0](*reducedTask[1])
+        task.__setstate__(reducedTask[2])
+        return task
+
+    def test_crash(self):
+        end, task = self.create_remote_tasklet()
+        try:
+            task = self.to_current_thread(task)
+            task.run()
+        finally:
+            end()
+
 
 
 #///////////////////////////////////////////////////////////////////////////////
