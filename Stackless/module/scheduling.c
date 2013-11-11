e8438c7181a1cbd8ee68b02c4d5dd11d5e947bd4
Kristjan Valur Jonsson <sweskman@gmail.com>
2013-11-11 12:07:33 +0000

    Fix SystemExit handling in interactive mode.  PyStackless_HandleSystemExit can return.

--- Stackless/module/scheduling.c
+++ Stackless/module/scheduling.c
@@ -1186,16 +1186,16 @@ tasklet_end(PyObject *retval)
             if (ts == slp_initial_tstate && !PyErr_ExceptionMatches(PyExc_TaskletExit)) {
                 handled = 1;
                 PyStackless_HandleSystemExit();
-            }
-            if (!ismain) {
+            } else if (!ismain) {
                 /* deal with TaskletExit on a non-main tasklet */
                 PyErr_Clear();
-                Py_INCREF(Py_None);
-                retval = Py_None;
                 handled = 1;
             }
         }
-        if (!handled) {
+        if (handled) {
+            Py_INCREF(Py_None);
+            retval = Py_None;
+        } else {
             if (!ismain) {
                 /* non-main tasklets get the chance to handle errors.
                  * errors in the handlers (or a non-present one)
