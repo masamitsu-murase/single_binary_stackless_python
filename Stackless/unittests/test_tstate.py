a5966f6e3fb31d8ca9233b5d2a6b034cee007a40
Anselm Kruis <a.kruis@science-computing.de>
2013-12-23 23:50:08 +0100

    Stackless issue #40. Fix tstate preservation, if an exception is present and tracing is enabled. https://bitbucket.org/stackless-dev/stackless/issue/40

--- Stackless/unittests/test_tstate.py
+++ Stackless/unittests/test_tstate.py
@@ -236,10 +236,8 @@ class TestTracingState(StacklessTestCase):
             self.assertEqual(ei[0], ZeroDivisionError)
             self.assertTrue("by zero" in str(ei[1]))
 
-    @unittest.skip("Implementation defect in C function slp_schedule_task_prepared()")
     def testExceptionAndTraceState(self):
         t = tasklet(self.Tasklet3)
-        sys.exc_clear()
         t()
         t.run()
         self.assertGreater(len(self.trace), 0)
