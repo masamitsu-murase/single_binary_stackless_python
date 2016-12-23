af19c312353f51f2f7c2013cc8536a4cf9a8bdf0
Anselm Kruis <a.kruis@science-computing.de>
2016-12-23 23:26:00 +0100

    Test suite cleanup: picklebug.py is no unit-test, move it to tests.

--- Stackless/test/picklebug.py
+++ Stackless/test/picklebug.py
@@ -1,24 +1,31 @@
+#
+# -*- coding: utf-8 -*-
+#
+
 """
 This file was needed to figure out a deep problem with generator pickling.
 I'm adding this file to the unittests as a template to cure similar
 problems in the future. This file only works in a debug build.
+
+Run with python -i ...
 """
+
+from __future__ import absolute_import, print_function, division
 
 import sys
 import gc
 import cPickle as pickle
 import os
-from stackless import *
+import stackless
 
 try:
     genschedoutertest  # @UndefinedVariable
 except NameError:
-    try:
-        execfile(os.path.join(os.path.dirname(__file__), "test_pickle.py"))
-    except SystemExit:
-        pass
+    sys.path.insert(0, os.path.normpath(os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                                                     os.pardir, "unittests")))
+    from test_pickle import genschedoutertest
 
-t = tasklet(genschedoutertest)(20, 13)  # @UndefinedVariable
+t = stackless.tasklet(genschedoutertest)(20, 13)  # @UndefinedVariable
 t.run()
 s = pickle.dumps(t)
 t.run()
@@ -31,7 +38,7 @@ gc.collect()
 pre = stackless._get_all_objects()
 post = pre[:]
 
-print "refs before unpickling, objects", sys.gettotalrefcount(), len(pre)
+print("refs before unpickling, objects", sys.gettotalrefcount(), len(pre))
 pickle.loads(s).run()
 post = None
 gc.collect()
@@ -41,8 +48,8 @@ for i, ob in enumerate(post):
         del post[i]
 del i, ob
 gc.collect()
-print "refs after  unpickling, objects", sys.gettotalrefcount(), len(post)
+print("refs after  unpickling, objects", sys.gettotalrefcount(), len(post))
 newob = post[:len(post) - len(pre)]
-print "look into newob"
+print("look into newob")
 del pre, post
 gc.collect()
