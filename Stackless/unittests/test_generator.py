578115c63e4f3da6d2c842903586a9f58f76fc33
hg <none@none>
2017-09-02 18:19:39 +0200

    Issue #130: fix pickling of exhausted generator objects
    Pickling of an exhausted generator no longer causes a SystemError.
    
    https://bitbucket.org/stackless-dev/stackless/issues/130

--- Stackless/unittests/test_generator.py
+++ Stackless/unittests/test_generator.py
@@ -3,6 +3,7 @@ import unittest
 import gc
 import stackless
 import types
+import pickle
 
 from support import test_main  # @UnusedImport
 from support import StacklessTestCase
@@ -55,6 +56,29 @@ class TestGeneratorWrapper(StacklessTestCase):
         self.assertIs(g0.gi_code, g1.gi_code)
         self.assertIsNot(g0.gi_frame, g1.gi_frame)
         self.assertEqual(g0.__name__, "exhausted_generator")
+
+
+class TestGeneratorPickling(StacklessTestCase):
+    def test_name(self):
+        def g():
+            yield 1
+        gen_orig = g()
+        p = pickle.dumps(gen_orig)
+        gen_new = pickle.loads(p)
+
+        self.assertEqual(gen_new.__name__, gen_orig.__name__)
+
+    def test_exhausted(self):
+        def g():
+            yield 1
+        gen_orig = g()
+        self.assertEqual(gen_orig.next(), 1)
+        self.assertRaises(StopIteration, gen_orig.next)
+        p = pickle.dumps(gen_orig)
+        self.assertRaises(StopIteration, gen_orig.next)
+        gen_new = pickle.loads(p)
+        self.assertRaises(StopIteration, gen_new.next)
+
 
 if __name__ == '__main__':
     unittest.main()
