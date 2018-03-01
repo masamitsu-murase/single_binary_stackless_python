88cfa4ac20ed1930462b9ac07b7157fe6950c02b
Masamitsu MURASE <masamitsu.murase@gmail.com>
2018-03-02 01:18:17 +0900

    Remove ctypes.

--- PC/config.c
+++ PC/config.c
@@ -193,7 +193,9 @@ struct _inittab _PyImport_Inittab[] = {
     {"_io", init_io},
 
     {"_bsddb", init_bsddb},
+#ifndef Py_BUILD_FOR_WINDOWS_PE
     {"_ctypes", init_ctypes},
+#endif
 //    {"_ctypes_test", init_ctypes_test},
     {"_elementtree", init_elementtree},
     {"_hashlib", init_hashlib},
