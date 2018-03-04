dee14be675273a074bb231e255aeca722b899830
Masamitsu MURASE <masamitsu.murase@gmail.com>
2018-03-04 18:59:04 +0900

    Fix syntax error in bat file.
    A space is necessary in "if" statement.

--- PCbuild/my_test.bat
+++ PCbuild/my_test.bat
@@ -36,7 +36,7 @@ if ERRORLEVEL 1 (
 )
 
 %PYTHON% -m test.regrtest -x test_argparse test_asynchat test_asyncore test_calendar test_class test_cmath test_class test_cmath test_codecs test_ctypes test_distutils test_email test_email_renamed test_ftplib test_hash test_json test_lib2to3 test_macpath test_ssl test_unittest
-if ERRORLEVEL 1(
+if ERRORLEVEL 1 (
     echo "regrtest failed."
     exit /b 1
 )
