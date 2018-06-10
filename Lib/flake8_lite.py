7c454eb6826f9d53b33c91b5cdf776ecbd0747a6
Masamitsu MURASE <masamitsu.murase@gmail.com>
2018-06-10 13:42:52 +0900

    Update flake8_lite to version 1.1.0.

--- Lib/flake8_lite.py
+++ Lib/flake8_lite.py
@@ -1,10 +1,30 @@
 # coding: utf-8
 
+# Copyright 2018 Masamitsu MURASE
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
 import sys
 import pyflakes.api
 import pycodestyle
 
-__version__ = "1.0.0"
+__version__ = "1.1.0"
 
 
 class PyflakesReporter(object):
@@ -203,8 +223,9 @@ def main():
     filtered_results = list(filter(lambda x: (x.code not in ignore_list),
                                    all_results))
     for i in filtered_results:
-        print(output_format % {"filename": i.filename, "row": i.lineno, "col": i.offset,
-                               "code": i.code, "text": i.text})
+        print(output_format % {"filename": i.filename, "row": i.lineno,
+                               "col": i.offset, "code": i.code,
+                               "text": i.text})
 
     if len(filtered_results) > 0:
         return 1
