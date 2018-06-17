648a3d6234c0da3b5e5d0b9a2c2f27f696d8a9e4
Masamitsu MURASE <masamitsu.murase@gmail.com>
2018-06-18 00:10:09 +0900

    Add exepy.

---
+++ Lib/exepy.py
@@ -0,0 +1,94 @@
+# coding: utf-8
+
+import argparse
+import os.path
+import shutil
+import struct
+import sys
+import zlib
+import ctypes
+import ctypes.wintypes as wintypes
+
+USER_SOURCE_ID = 100
+RT_RCDATA = 10
+if ctypes.sizeof(ctypes.c_void_p) == ctypes.sizeof(ctypes.c_ulonglong):
+    ULONG_PTR = ctypes.c_ulonglong
+elif ctypes.sizeof(ctypes.c_void_p) == ctypes.sizeof(ctypes.c_ulong):
+    ULONG_PTR = ctypes.c_ulong
+else:
+    raise NotImplementedError("No ULONG_PTR")
+
+
+def update_resource(resource_id, data, exe_dll_filename, lang_id=0,
+                    data_type=RT_RCDATA):
+    begin_update_resource = ctypes.windll.kernel32.BeginUpdateResourceW
+    begin_update_resource.argtypes = [wintypes.LPWSTR, wintypes.BOOL]
+    begin_update_resource.restype = wintypes.HANDLE
+    handle = begin_update_resource(exe_dll_filename, False)
+    if handle == 0:
+        raise RuntimeError("BeginUpdateResource failure")
+
+    update_resource_api = ctypes.windll.kernel32.UpdateResourceW
+    update_resource_api.argtypes = [wintypes.HANDLE, ULONG_PTR, ULONG_PTR,
+                                    wintypes.WORD, wintypes.LPVOID,
+                                    wintypes.DWORD]
+    update_resource_api.restype = wintypes.BOOL
+    c_buffer = ctypes.create_string_buffer(data)
+    result = update_resource_api(handle, data_type, resource_id, lang_id,
+                                 c_buffer, len(data))
+    if result == 0:
+        raise RuntimeError("UpdateResource failure")
+
+    end_update_resource = ctypes.windll.kernel32.EndUpdateResourceW
+    end_update_resource.argtypes = [wintypes.HANDLE, wintypes.BOOL]
+    end_update_resource.restype = wintypes.BOOL
+    result = end_update_resource(handle, False)
+    if result == 0:
+        raise RuntimeError("EndUpdateResource failure")
+
+
+# Data structure
+#   uint32_t compressed_size;
+#   uint32_t uncompressed_size;
+#   uint32_t file_count;
+#   uint32_t [] file_offset;
+#   unsigned char [] compressed_file_data;  // separated by '\0'
+#   char [] file_name;  // separated by '\0'.
+def create_resource_bin(input_filename_list):
+    data = b""
+    file_offset = []
+    file_name = b""
+    for input_filename in input_filename_list:
+        with open(input_filename, "rb") as file:
+            file_offset.append(len(data))
+            data += file.read() + b"\0"
+            file_name += input_filename.encode("ascii") + b"\0"
+    compressed_data = zlib.compress(data, 9)
+
+    return struct.pack("=LLL%dL" % len(file_offset), len(compressed_data),
+                       len(data), len(file_offset), *file_offset) \
+        + compressed_data + file_name
+
+
+def usage():
+    print("Usage: python -m exepy output.exe input.py [input2.py ...]")
+
+
+def main():
+    if len(sys.argv) < 3:
+        usage()
+        sys.exit(1)
+
+    output_filename = sys.argv[1]
+    input_filename_list = sys.argv[2:]
+    if os.path.exists(output_filename):
+        print("Specify a new file as output.exe.")
+        sys.exit(2)
+    resource_bin = create_resource_bin(input_filename_list)
+    shutil.copy(sys.executable, output_filename)
+    update_resource(USER_SOURCE_ID, resource_bin,
+                    os.path.abspath(output_filename))
+
+
+if __name__ == "__main__":
+    main()
