e5ee980b3c47d8bd4d9fdf1d49a7fedcfa823826
Masamitsu MURASE <masamitsu.murase@gmail.com>
2018-06-18 16:07:28 +0900

    Set default command line.

--- Lib/exepy.py
+++ Lib/exepy.py
@@ -5,12 +5,14 @@ import os.path
 import shutil
 import struct
 import sys
+import time
 import zlib
 import ctypes
 import ctypes.wintypes as wintypes
 
 USER_SOURCE_ID = 100
 USER_COMMAND_ID = 101
+
 RT_RCDATA = 10
 if ctypes.sizeof(ctypes.c_void_p) == ctypes.sizeof(ctypes.c_ulonglong):
     ULONG_PTR = ctypes.c_ulonglong
@@ -20,32 +22,50 @@ else:
     raise NotImplementedError("No ULONG_PTR")
 
 
-def update_resource(resource_id, data, exe_dll_filename, lang_id=0,
-                    data_type=RT_RCDATA):
-    begin_update_resource = ctypes.windll.kernel32.BeginUpdateResourceW
-    begin_update_resource.argtypes = [wintypes.LPWSTR, wintypes.BOOL]
-    begin_update_resource.restype = wintypes.HANDLE
-    handle = begin_update_resource(exe_dll_filename, False)
-    if handle == 0:
-        raise RuntimeError("BeginUpdateResource failure")
+class ExeResourceUpdater(object):
+    def __init__(self, exe_dll_filename):
+        self.exe_dll_filename = exe_dll_filename
+        self.handle = None
 
-    update_resource_api = ctypes.windll.kernel32.UpdateResourceW
-    update_resource_api.argtypes = [wintypes.HANDLE, ULONG_PTR, ULONG_PTR,
-                                    wintypes.WORD, wintypes.LPVOID,
-                                    wintypes.DWORD]
-    update_resource_api.restype = wintypes.BOOL
-    c_buffer = ctypes.create_string_buffer(data)
-    result = update_resource_api(handle, data_type, resource_id, lang_id,
-                                 c_buffer, len(data))
-    if result == 0:
-        raise RuntimeError("UpdateResource failure")
+    def __enter__(self):
+        begin_update_resource = ctypes.windll.kernel32.BeginUpdateResourceW
+        begin_update_resource.argtypes = [wintypes.LPWSTR, wintypes.BOOL]
+        begin_update_resource.restype = wintypes.HANDLE
+        handle = begin_update_resource(self.exe_dll_filename, False)
+        if handle == 0:
+            raise RuntimeError("BeginUpdateResource failure")
+        self.handle = handle
+        return self
 
-    end_update_resource = ctypes.windll.kernel32.EndUpdateResourceW
-    end_update_resource.argtypes = [wintypes.HANDLE, wintypes.BOOL]
-    end_update_resource.restype = wintypes.BOOL
-    result = end_update_resource(handle, False)
-    if result == 0:
-        raise RuntimeError("EndUpdateResource failure")
+    def update(self, resource_id, data, lang_id=0, data_type=RT_RCDATA):
+        update_resource_api = ctypes.windll.kernel32.UpdateResourceW
+        update_resource_api.argtypes = [wintypes.HANDLE, ULONG_PTR, ULONG_PTR,
+                                        wintypes.WORD, wintypes.LPVOID,
+                                        wintypes.DWORD]
+        update_resource_api.restype = wintypes.BOOL
+        c_buffer = ctypes.create_string_buffer(data)
+        result = update_resource_api(self.handle, data_type, resource_id,
+                                     lang_id, c_buffer, len(data))
+        if result == 0:
+            raise RuntimeError("UpdateResource failure")
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        if self.handle is None:
+            return
+
+        try:
+            end_update_resource = ctypes.windll.kernel32.EndUpdateResourceW
+            end_update_resource.argtypes = [wintypes.HANDLE, wintypes.BOOL]
+            end_update_resource.restype = wintypes.BOOL
+            result = end_update_resource(self.handle, False)
+            if result == 0:
+                error_code = ctypes.windll.kernel32.GetLastError()
+                raise RuntimeError("EndUpdateResource failure: %d" % error_code)
+        finally:
+            close_handle = ctypes.windll.kernel32.CloseHandle
+            close_handle.argtypes = [wintypes.HANDLE]
+            close_handle.restype = wintypes.BOOL
+            close_handle(self.handle)
 
 
 # Data structure
@@ -98,10 +118,10 @@ def check_args(output_filename, input_filename):
 def create_command_resource_bin(commands):
     # uint32_t  argc
     # char []   argv
-    command_str = "".join((x + "\0") for x in commands) + "\0"
-    command_wchar_array = ctypes.create_unicode_buffer(command_str)
-    command_array = ctypes.create_string_buffer(ctypes.sizeof(command_wchar_array))
-    ctypes.memmove(command_array, command_wchar_array, ctypes.sizeof(command_wchar_array))
+    command_str = "".join((x + "\0") for x in commands)
+    cmd_ws_array = ctypes.create_unicode_buffer(command_str)
+    command_array = ctypes.create_string_buffer(ctypes.sizeof(cmd_ws_array))
+    ctypes.memmove(command_array, cmd_ws_array, ctypes.sizeof(cmd_ws_array))
     return struct.pack("=L", len(commands)) + command_array.raw
 
 
@@ -116,15 +136,31 @@ def main():
     commands = ("-m", main_module)
     command_resource = create_command_resource_bin(commands)
     try:
-        print("Generating...")
-        shutil.copy(sys.executable, output_filename)
-        update_resource(USER_SOURCE_ID, resource_bin,
-                        os.path.abspath(output_filename))
-        update_resource(USER_COMMAND_ID, command_resource,
-                        os.path.abspath(output_filename))
-        print("Done.")
+        sys.stdout.write("Generating.")
+        sys.stdout.flush()
+        retry_max_count = 10
+        for i in range(retry_max_count):
+            try:
+                shutil.copy(sys.executable, output_filename)
+                with ExeResourceUpdater(os.path.abspath(output_filename)) as updater:
+                    updater.update(USER_SOURCE_ID, resource_bin)
+                    updater.update(USER_COMMAND_ID, command_resource)
+                    time.sleep(0.3)
+            except RuntimeError:
+                if i != retry_max_count - 1:
+                    sys.stdout.write(".")
+                    sys.stdout.flush()
+                    time.sleep(0.1)
+                else:
+                    raise
+            else:
+                break
+        print("\nDone.")
     except Exception:
-        os.remove(output_filename)
+        try:
+            os.remove(output_filename)
+        except Exception:
+            pass
         raise
 
 
