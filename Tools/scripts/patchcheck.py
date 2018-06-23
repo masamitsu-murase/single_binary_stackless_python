b248ae81387d51306f3cae563bb609cf636a165d
Anselm Kruis <anselm.kruis@atos.net>
2018-06-23 23:49:31 +0200

    Stackless issue #161: Add Stackless support to patchcheck.py
    Now Tools/scripts/patchcheck.py uses Stackless specific git base
    branches for Stackless source trees (i.e. "master-slp" instead of
    "master")
    (cherry picked from commit a4e7d75b3a985e1739b8c4a90cc124e2e1b29012)

--- Tools/scripts/patchcheck.py
+++ Tools/scripts/patchcheck.py
@@ -10,6 +10,10 @@ import sysconfig
 import reindent
 import untabify
 
+try:
+    import stackless
+except ImportError:
+    stackless = None
 
 # Excluded directories which are copies of external libraries:
 # don't check their coding style
@@ -43,6 +47,12 @@ def status(message, modal=False, info=None):
             return result
         return call_fxn
     return decorated_fxn
+
+
+def is_stackless():
+    """Check if this is a Stackless Python source"""
+    return (stackless is not None and
+            os.path.exists(os.path.join(SRCDIR, 'Stackless')))
 
 
 def mq_patches_applied():
@@ -88,6 +98,8 @@ def get_base_branch():
         base_branch = "master"
     else:
         base_branch = "{0.major}.{0.minor}".format(version)
+    if is_stackless():
+        base_branch += "-slp"
     this_branch = get_git_branch()
     if this_branch is None or this_branch == base_branch:
         # Not on a git PR branch, so there's no base branch
