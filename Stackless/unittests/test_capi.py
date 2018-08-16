stackless-dev/cpython

Stackless issue #166: Fix PyEval_EvalFrameEx()
Support using PyEval_EvalFrameEx() without a main tasklet. And fix a
potential reference counting problem.

Stackless/unittests/test_capi.py
@@ -23,12 +23,23 @@ Tests not relevant for pure Python code.
 from __future__ import print_function, absolute_import, division
 
 import inspect
+import io
 import stackless
 import sys
+import time
+import traceback
 import unittest
 
 from support import test_main  # @UnusedImport
-from support import StacklessTestCase
+from support import StacklessTestCase, withThreads, require_one_thread
+
+if withThreads:
+    try:
+        import thread as _thread
+    except ImportError:
+        import _thread
+else:
+    _thread = None
 
 
 class Test_PyEval_EvalFrameEx(StacklessTestCase):
@@ -92,6 +103,63 @@ class Test_PyEval_EvalFrameEx(StacklessTestCase):
         result = stackless.test_PyEval_EvalFrameEx(f.__code__, f.__globals__)
         self.assertTrue(self.other_done)
         self.assertEqual(result, 666)
+
+    def test_throw(self):
+        def f():
+            raise RuntimeError("must not be called")
+
+        try:
+            exc = ZeroDivisionError("test")
+            stackless.test_PyEval_EvalFrameEx(f.__code__, f.__globals__, (), throw=exc)
+        except ZeroDivisionError:
+            e, tb = sys.exc_info()[1:]
+            self.assertIs(e, exc)
+            self.assertEqual(traceback.extract_tb(tb)[-1][2], f.__name__)
+        else:
+            self.fail("expected exception")
+
+    @unittest.skipUnless(withThreads, "test requires threading")
+    def test_no_main_tasklet(self):
+        def f(self, lock):
+            self.thread_done = True
+            lock.release()
+
+        def g(func, *args, **kw):
+            return func(*args, **kw)
+
+        lock = _thread.allocate_lock()
+
+        # first a dry run to check the threading logic
+        lock.acquire()
+        self.thread_done = False
+        _thread.start_new_thread(g, (stackless.test_PyEval_EvalFrameEx, f.__code__, f.__globals__, (self, lock)))
+        lock.acquire()
+        self.assertTrue(self.thread_done)
+        # return  # uncomment to skip the real test
+        # and now the real smoke test: now PyEval_EvalFrameEx gets called without a main tasklet
+        self.thread_done = False
+        _thread.start_new_thread(stackless.test_PyEval_EvalFrameEx, (f.__code__, f.__globals__, (self, lock)))
+        lock.acquire()
+        self.assertTrue(self.thread_done)
+
+    @unittest.skipUnless(withThreads, "test requires threading")
+    @require_one_thread  # we fiddle with sys.stderr
+    def test_throw_no_main_tasklet(self):
+        def f():
+            raise RuntimeError("must not be called")
+
+        exc = ZeroDivisionError("test")
+        saved_sys_stderr = sys.stderr
+        sys.stderr = iobuffer = io.BytesIO()
+        try:
+            _thread.start_new_thread(stackless.test_PyEval_EvalFrameEx, (f.__code__, f.__globals__, ()), dict(throw=exc))
+            time.sleep(0.1)
+        finally:
+            sys.stderr = saved_sys_stderr
+        msg = str(iobuffer.getvalue())
+        self.assertNotIn("Pending error while entering Stackless subsystem", msg)
+        self.assertIn(str(exc), msg)
+        #print(msg)
 
 
 if __name__ == "__main__":

masamitsu-murase/single_binary_stackless_python

(cherry picked from commit 187184ba0c4f7250387efb365a651358bca3ebfa)

Stackless/unittests/test_capi.py
@@ -23,12 +23,23 @@ Tests not relevant for pure Python code.
 from __future__ import print_function, absolute_import, division
 
 import inspect
+import io
 import stackless
 import sys
+import time
+import traceback
 import unittest
 
 from support import test_main  # @UnusedImport
-from support import StacklessTestCase
+from support import StacklessTestCase, withThreads, require_one_thread
+
+if withThreads:
+    try:
+        import thread as _thread
+    except ImportError:
+        import _thread
+else:
+    _thread = None
 
 
 class Test_PyEval_EvalFrameEx(StacklessTestCase):
@@ -92,6 +103,63 @@ class Test_PyEval_EvalFrameEx(StacklessTestCase):
         result = stackless.test_PyEval_EvalFrameEx(f.__code__, f.__globals__)
         self.assertTrue(self.other_done)
         self.assertEqual(result, 666)
+
+    def test_throw(self):
+        def f():
+            raise RuntimeError("must not be called")
+
+        try:
+            exc = ZeroDivisionError("test")
+            stackless.test_PyEval_EvalFrameEx(f.__code__, f.__globals__, (), throw=exc)
+        except ZeroDivisionError:
+            e, tb = sys.exc_info()[1:]
+            self.assertIs(e, exc)
+            self.assertEqual(traceback.extract_tb(tb)[-1][2], f.__name__)
+        else:
+            self.fail("expected exception")
+
+    @unittest.skipUnless(withThreads, "test requires threading")
+    def test_no_main_tasklet(self):
+        def f(self, lock):
+            self.thread_done = True
+            lock.release()
+
+        def g(func, *args, **kw):
+            return func(*args, **kw)
+
+        lock = _thread.allocate_lock()
+
+        # first a dry run to check the threading logic
+        lock.acquire()
+        self.thread_done = False
+        _thread.start_new_thread(g, (stackless.test_PyEval_EvalFrameEx, f.__code__, f.__globals__, (self, lock)))
+        lock.acquire()
+        self.assertTrue(self.thread_done)
+        # return  # uncomment to skip the real test
+        # and now the real smoke test: now PyEval_EvalFrameEx gets called without a main tasklet
+        self.thread_done = False
+        _thread.start_new_thread(stackless.test_PyEval_EvalFrameEx, (f.__code__, f.__globals__, (self, lock)))
+        lock.acquire()
+        self.assertTrue(self.thread_done)
+
+    @unittest.skipUnless(withThreads, "test requires threading")
+    @require_one_thread  # we fiddle with sys.stderr
+    def test_throw_no_main_tasklet(self):
+        def f():
+            raise RuntimeError("must not be called")
+
+        exc = ZeroDivisionError("test")
+        saved_sys_stderr = sys.stderr
+        sys.stderr = iobuffer = io.StringIO()
+        try:
+            _thread.start_new_thread(stackless.test_PyEval_EvalFrameEx, (f.__code__, f.__globals__, ()), dict(throw=exc))
+            time.sleep(0.1)
+        finally:
+            sys.stderr = saved_sys_stderr
+        msg = str(iobuffer.getvalue())
+        self.assertNotIn("Pending error while entering Stackless subsystem", msg)
+        self.assertIn(str(exc), msg)
+        #print(msg)
 
 
 if __name__ == "__main__":