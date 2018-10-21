b0ff085faccc4482d81b90218843278213b8caf9
Masamitsu MURASE <masamitsu.murase@gmail.com>
2018-10-22 07:42:01 +0900

    Implement exepy 2.1.0
    - Do not output message during test.
    - Suppress output during test.
    - Allow exepy to include arbitrary files.
    - Exclude directory from resource.
    - Test for embedded binary.
    - Fix exepy extract.
    - Fix test.
    - Add universal_newlines option.
    - For potential bug of azure pipelines.

--- PCbuild/my_test/test_exepy.py
+++ PCbuild/my_test/test_exepy.py
@@ -31,6 +31,26 @@ class TestExepy(unittest.TestCase):
         print("file2")
     """)
 
+    RESOURCE_FILE_CONTENT1 = textwrap.dedent("""\
+    import dir
+    import os
+
+    data = __loader__.get_data(os.path.join(os.path.dirname(__file__), "image.bin"))
+    if data == b"\x01\x02\x03\x04\x05\x06":
+        print("ok")
+    else:
+        print("ng")
+    """)
+    RESOURCE_FILE_CONTENT2 = textwrap.dedent("""\
+    import os
+
+    data = __loader__.get_data(os.path.join(os.path.dirname(__file__), "image.bin"))
+    if data == b"\x01\x02\x03\x04\x05\x06" * 2:
+        print("ok")
+    else:
+        print("ng")
+    """)
+
     def create_single_test_file(self, name):
         with open(name, "w") as file:
             file.write(self.SINGLE_FILE_CONTENT)
@@ -41,6 +61,18 @@ class TestExepy(unittest.TestCase):
         with open(self.SECOND_FILE_NAME, "w") as file:
             file.write(self.DOUBLE_FILE_CONTENT2)
 
+    def create_resource_test_file(self, name):
+        with open(name, "w") as file:
+            file.write(self.RESOURCE_FILE_CONTENT1)
+        with open("image.bin", "wb") as file:
+            file.write(b"\x01\x02\x03\x04\x05\x06")
+        os.mkdir("dir")
+        os.makedirs("dir/dir")
+        with open("dir/__init__.py", "w") as file:
+            file.write(self.RESOURCE_FILE_CONTENT2)
+        with open("dir/image.bin", "wb") as file:
+            file.write(b"\x01\x02\x03\x04\x05\x06" * 2)
+
     def test_create_exe(self):
         os.mkdir(self.TEMPDIR)
         pwd = os.path.abspath(os.curdir)
@@ -50,27 +82,55 @@ class TestExepy(unittest.TestCase):
             exename = "hoge.exe"
             self.create_single_test_file(filename)
             cmd = [sys.executable, "-m", "exepy", "create", exename, filename]
-            subprocess.check_call(cmd)
+            subprocess.check_call(cmd, stdout=subprocess.DEVNULL)
+            shutil.copy(exename, "..")  # For potential bug in azure-pipelines
             os.remove(filename)
             output = subprocess.check_output(exename, universal_newlines=True)
             self.assertEqual(output, "1\n")
 
             cmd = [sys.executable, "-m", "exepy", "extract", exename]
-            subprocess.check_call(cmd)
+            subprocess.check_call(cmd, stdout=subprocess.DEVNULL)
             with open(filename, "r") as file:
                 content = file.read()
             self.assertEqual(content, self.SINGLE_FILE_CONTENT)
 
             cmd = [sys.executable, "-m", "exepy", "extract", exename]
-            returncode = subprocess.call(cmd)
+            returncode = subprocess.call(cmd, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
             self.assertNotEqual(returncode, 0)
 
             cmd = [sys.executable, "-m", "exepy", "extract", "-f", exename]
-            returncode = subprocess.call(cmd)
+            returncode = subprocess.call(cmd, stdout=subprocess.DEVNULL)
             self.assertEqual(returncode, 0)
         finally:
             os.chdir(pwd)
             shutil.rmtree(self.TEMPDIR)
+            if os.path.isfile(exename):
+                os.remove(exename)
+
+    def test_nocommand(self):
+        os.mkdir(self.TEMPDIR)
+        pwd = os.path.abspath(os.curdir)
+        os.chdir(self.TEMPDIR)
+        try:
+            filename = "hoge.py"
+            exename = "hoge.exe"
+            self.create_single_test_file(filename)
+            cmd = [sys.executable, "-m", "exepy", "create", "--nocommand", exename, filename]
+            subprocess.check_call(cmd, stdout=subprocess.DEVNULL)
+            shutil.copy(exename, "..")  # For potential bug in azure-pipelines
+            os.remove(filename)
+
+            cmd = [sys.executable, "-m", "exepy", "extract", exename]
+            output = subprocess.check_output(cmd, universal_newlines=True)
+            with open(filename, "r") as file:
+                content = file.read()
+            self.assertEqual(content, self.SINGLE_FILE_CONTENT)
+            self.assertIn("no argv", output)
+        finally:
+            os.chdir(pwd)
+            shutil.rmtree(self.TEMPDIR)
+            if os.path.isfile(exename):
+                os.remove(exename)
 
     def test_2_files(self):
         os.mkdir(self.TEMPDIR)
@@ -81,14 +141,15 @@ class TestExepy(unittest.TestCase):
             exename = "hoge.exe"
             self.create_double_test_file(filename)
             cmd = [sys.executable, "-m", "exepy", "c", exename, filename, self.SECOND_FILE_NAME]
-            subprocess.check_call(cmd)
+            subprocess.check_call(cmd, stdout=subprocess.DEVNULL)
+            shutil.copy(exename, "..")  # For potential bug in azure-pipelines
             for file in glob.iglob("*.py"):
                 os.remove(file)
             output = subprocess.check_output(exename, universal_newlines=True)
             self.assertEqual(output, "file2\n")
 
             cmd = [sys.executable, "-m", "exepy", "e", exename]
-            subprocess.check_call(cmd)
+            subprocess.check_call(cmd, stdout=subprocess.DEVNULL)
             with open(filename, "r") as file:
                 content1 = file.read()
             with open(self.SECOND_FILE_NAME, "r") as file:
@@ -98,6 +159,30 @@ class TestExepy(unittest.TestCase):
         finally:
             os.chdir(pwd)
             shutil.rmtree(self.TEMPDIR)
+            if os.path.isfile(exename):
+                os.remove(exename)
+
+    def test_resource(self):
+        os.mkdir(self.TEMPDIR)
+        pwd = os.path.abspath(os.curdir)
+        os.chdir(self.TEMPDIR)
+        try:
+            filename = "hoge.py"
+            exename = "hoge.exe"
+            self.create_resource_test_file(filename)
+            cmd = [sys.executable, "-m", "exepy", "create", exename, filename, "image.bin", "dir"]
+            subprocess.check_call(cmd, stdout=subprocess.DEVNULL)
+            shutil.copy(exename, "..")  # For potential bug in azure-pipelines
+            os.remove(filename)
+            os.remove("image.bin")
+            shutil.rmtree("dir")
+            output = subprocess.check_output([exename], universal_newlines=True)
+            self.assertEqual(output, "ok\nok\n")
+        finally:
+            os.chdir(pwd)
+            shutil.rmtree(self.TEMPDIR)
+            if os.path.isfile(exename):
+                os.remove(exename)
 
     def test_argument(self):
         os.mkdir(self.TEMPDIR)
@@ -108,13 +193,16 @@ class TestExepy(unittest.TestCase):
             exename = "hoge.exe"
             self.create_single_test_file(filename)
             cmd = [sys.executable, "-m", "exepy", "create", exename, filename]
-            subprocess.check_call(cmd)
+            subprocess.check_call(cmd, stdout=subprocess.DEVNULL)
+            shutil.copy(exename, "..")  # For potential bug in azure-pipelines
             os.remove(filename)
             output = subprocess.check_output([exename, "arg1", "arg2"], universal_newlines=True)
             self.assertEqual(output, "3\n")
         finally:
             os.chdir(pwd)
             shutil.rmtree(self.TEMPDIR)
+            if os.path.isfile(exename):
+                os.remove(exename)
 
 
 if __name__ == "__main__":
