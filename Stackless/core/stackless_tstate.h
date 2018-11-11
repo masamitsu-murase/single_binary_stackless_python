masamitsu-murase/single_binary_stackless_python

commit 0727a8ec807f664d16855b4add144a7357181842

Stackless issue #152: Move globals to PyStacklessInterpreterState
Removed globals:
- slp_initial_tstate
- channel_hook
- slp_error_handler
- _slp_schedule_fasthook
- _slp_schedule_hook
- slp_cstack_chain
- mem_bomb

Additionally I optimized the ordering of the fields of PyStacklessState
and PyStacklessInterpreterState to avoid padding.

Stackless/core/stackless_tstate.h
@@ -11,22 +11,44 @@
 #endif
 #endif
 
+typedef struct _cstack PyCStackObject;
+typedef struct _bomb PyBombObject;
+typedef struct _tasklet PyTaskletObject;
+typedef int (slp_schedule_hook_func) (PyTaskletObject *from, PyTaskletObject *to);
+typedef struct _ts PyThreadState;
 typedef struct {
-    PyObject * reduce_frame_func;
+    PyCStackObject * cstack_chain;              /* the chain of all C-stacks of this interpreter. This is an uncounted/borrowed ref. */
+    PyObject * reduce_frame_func;               /* a function used to pickle frames */
+    PyObject * error_handler;                   /* the Stackless error handler */
+    PyObject * channel_hook;                    /* the channel callback function */
+    PyBombObject * mem_bomb;                    /* a permanent bomb to use for memory errors */
+    PyObject * schedule_hook;                   /* the schedule callback function */
+    slp_schedule_hook_func * schedule_fasthook; /* the fast C-only schedule_hook */
+    PyThreadState * initial_tstate;             /* recording the main thread state */
 } PyStacklessInterpreterState;
 
-#define SPL_INTERPRETERSTATE_NEW(interp) \
-    (interp)->st.reduce_frame_func = NULL
+#define SPL_INTERPRETERSTATE_NEW(interp)       \
+    (interp)->st.cstack_chain = NULL;          \
+    (interp)->st.reduce_frame_func = NULL;     \
+    (interp)->st.error_handler = NULL;         \
+    (interp)->st.channel_hook = NULL;          \
+    (interp)->st.mem_bomb = NULL;              \
+    (interp)->st.schedule_hook = NULL;         \
+    (interp)->st.schedule_fasthook = NULL;     \
+    (interp)->st.initial_tstate = NULL
 
-#define SPL_INTERPRETERSTATE_CLEAR(interp) \
-	Py_CLEAR((interp)->st.reduce_frame_func)
-
+#define SPL_INTERPRETERSTATE_CLEAR(interp)     \
+    (interp)->st.cstack_chain = NULL; /* uncounted ref */  \
+    Py_CLEAR((interp)->st.reduce_frame_func);  \
+    Py_CLEAR((interp)->st.error_handler);      \
+    Py_CLEAR((interp)->st.mem_bomb);           \
+    Py_CLEAR((interp)->st.channel_hook);       \
+    Py_CLEAR((interp)->st.schedule_hook);      \
+    (interp)->st.schedule_fasthook = NULL
 
 struct _frame; /* Avoid including frameobject.h */
 
 typedef struct _sts {
-    /* the blueprint for new stacks */
-    struct _cstack *initial_stub;
     /* "serial" is incremented each time we create a new stub.
      * (enter "stackless" from the outside)
      * and "serial_last_jump" indicates to which stub the current
@@ -40,6 +62,8 @@ typedef struct _sts {
     long serial;
     long serial_last_jump;
 #endif
+    /* the blueprint for new stacks */
+    struct _cstack *initial_stub;
     /* the base address for hijacking stacks. XXX deprecating */
     intptr_t *cstack_base;
     /* stack overflow check and init flag */

Stackless/core/stackless_impl.h
@@ -353,8 +353,6 @@ PyObject * slp_wrap_call_frame(PyFrameObject *frame, int exc, PyObject *retval);
 PyAPI_DATA(int) slp_enable_softswitch;
 PyAPI_DATA(int) slp_try_stackless;
 
-extern PyCStackObject * slp_cstack_chain;
-
 PyCStackObject * slp_cstack_new(PyCStackObject **cst, intptr_t *stackref, PyTaskletObject *task);
 size_t slp_cstack_save(PyCStackObject *cstprev);
 void slp_cstack_restore(PyCStackObject *cst);
@@ -667,9 +665,6 @@ void slp_channel_remove_slow(PyTaskletObject *task,
                              PyChannelObject **u_chan,
                              int *dir, PyTaskletObject **next);
 
-/* recording the main thread state */
-extern PyThreadState * slp_initial_tstate;
-
 /* protecting soft-switched tasklets in other threads */
 int slp_ensure_linkage(PyTaskletObject *task);
 
@@ -752,6 +747,7 @@ PyObject * slp_nomemory_bomb(void);
 PyObject * slp_bomb_explode(PyObject *bomb);
 int slp_init_bombtype(void);
 PyObject * slp_get_obj_for_exc_state(_PyErr_StackItem *exc_info);
+PyBombObject * slp_new_bomb(void);
 
 /* handy abbrevations */
 
@@ -802,10 +798,6 @@ int slp_pickle_with_tracing_state(void);
 
 /* debugging/monitoring */
 
-typedef int (slp_schedule_hook_func) (PyTaskletObject *from,
-                                       PyTaskletObject *to);
-extern slp_schedule_hook_func* _slp_schedule_fasthook;
-extern PyObject* _slp_schedule_hook;
 int slp_schedule_callback(PyTaskletObject *prev, PyTaskletObject *next);
 
 int slp_prepare_slots(PyTypeObject*);

Stackless/core/stacklesseval.c
@@ -32,9 +32,6 @@ int slp_enable_softswitch = 1;
  */
 int slp_try_stackless = 0;
 
-/* the list of all stacks of all threads */
-struct _cstack *slp_cstack_chain = NULL;
-
 
 /******************************************************
 
@@ -64,8 +61,9 @@ static void slp_cstack_cacheclear(void)
 static void
 cstack_dealloc(PyCStackObject *cst)
 {
-    slp_cstack_chain = cst;
-    SLP_CHAIN_REMOVE(PyCStackObject, &slp_cstack_chain, cst, next,
+    PyThreadState * ts = PyThreadState_GET();
+    ts->interp->st.cstack_chain = cst;
+    SLP_CHAIN_REMOVE(PyCStackObject, &ts->interp->st.cstack_chain, cst, next,
                      prev);
 #ifdef Py_REF_DEBUG
     PyObject_Del(cst);
@@ -125,7 +123,7 @@ slp_cstack_new(PyCStackObject **cst, intptr_t *stackref, PyTaskletObject *task)
 
     (*cst)->startaddr = stackbase;
     (*cst)->next = (*cst)->prev = NULL;
-    SLP_CHAIN_INSERT(PyCStackObject, &slp_cstack_chain, *cst, next, prev);
+    SLP_CHAIN_INSERT(PyCStackObject, &ts->interp->st.cstack_chain, *cst, next, prev);
     (*cst)->serial = ts->st.serial_last_jump;
     (*cst)->task = task;
     (*cst)->tstate = ts;
@@ -339,6 +337,12 @@ slp_eval_frame(PyFrameObject *f)
 
     if (fprev == NULL && ts->st.main == NULL) {
         int returning;
+        if (ts->interp->st.mem_bomb == NULL) {
+            /* allocate the permanent bomb to use for mem errors */
+            if ((ts->interp->st.mem_bomb = slp_new_bomb()) == NULL)
+                return NULL;
+        }
+
         /* this is the initial frame, so mark the stack base */
 
         /*
@@ -371,8 +375,10 @@ slp_eval_frame(PyFrameObject *f)
         if (returning != 1) {
             assert(f == SLP_PEEK_NEXT_FRAME(ts));
         }
+        assert(ts->interp->st.mem_bomb != NULL);
         return slp_run_tasklet();
     }
+    assert(ts->interp->st.mem_bomb != NULL); /* see above */
     Py_INCREF(Py_None);
     Py_XINCREF(fprev);
     retval = slp_frame_dispatch(f, fprev, 0, Py_None);
@@ -482,8 +488,10 @@ run_other_threads(PyObject **sleep, Py_ssize_t count)
 void slp_kill_tasks_with_stacks(PyThreadState *target_ts)
 {
     PyThreadState *cts = PyThreadState_GET();
+    PyInterpreterState * interp = cts->interp;
     int in_loop = 0;
 
+    assert(target_ts == NULL || interp == target_ts->interp);
     if (target_ts == NULL || target_ts == cts) {
         /* Step I of III
          * A loop to kill tasklets on the current thread.
@@ -495,7 +503,7 @@ void slp_kill_tasks_with_stacks(PyThreadState *target_ts)
          *    tstate of all cstacks, which still belong to the killed tasklet.
          */
         while (1) {
-            PyCStackObject *csfirst = slp_cstack_chain, *cs;
+            PyCStackObject *csfirst = interp->st.cstack_chain, *cs;
             PyTaskletObject *t;
 
             if (csfirst == NULL)
@@ -575,7 +583,7 @@ void slp_kill_tasks_with_stacks(PyThreadState *target_ts)
             } /* already dead? */
 
             /* Now remove the tstate from all cstacks of tasklet t */
-            csfirst = slp_cstack_chain;
+            csfirst = interp->st.cstack_chain;
             if (csfirst != NULL) {
                 in_loop = 0;
                 for (cs = csfirst; ; cs = cs->next) {
@@ -624,7 +632,7 @@ other_threads:
 
             /* build a list of tasklets to be killed */
             SLP_HEAD_LOCK();
-            for (ts = cts->interp->tstate_head; ts != NULL; ts = ts->next) {
+            for (ts = interp->tstate_head; ts != NULL; ts = ts->next) {
                 if (ts != cts) {
                     /* Inactivate thread ts. In case the thread is active,
                      * it will be killed. If the thread is sleping, it
@@ -657,7 +665,7 @@ other_threads:
 
         /* other threads, second pass: kill tasklets with nesting-level > 0 and
          * clear tstate if target_ts != NULL && target_ts != cts. */
-        if (slp_cstack_chain == NULL) {
+        if (interp->st.cstack_chain == NULL) {
             Py_XDECREF(sleepfunc);
             goto current_main;
         }
@@ -671,7 +679,7 @@ other_threads:
          * Steps 1 and 2 must not run Python code (release the GIL), because another thread could
          * modify slp_cstack_chain.
          */
-        for(cs = slp_cstack_chain; cs != slp_cstack_chain || in_loop == 0; cs = cs->next) {
+        for(cs = interp->st.cstack_chain; cs != interp->st.cstack_chain || in_loop == 0; cs = cs->next) {
             /* Count tasklets to be killed.
              * This loop body must not release the GIL
              */
@@ -697,15 +705,15 @@ other_threads:
                 count++;
             }
         }
-        assert(cs == slp_cstack_chain);
+        assert(cs == interp->st.cstack_chain);
         if (count > 0) {
             PyObject *tasklets = PyTuple_New(count);
             if (NULL == tasklets) {
                 PyErr_Print();
                 return;
             }
-            assert(cs == slp_cstack_chain);
-            for(in_loop = 0, count=0; cs != slp_cstack_chain || in_loop == 0; cs = cs->next) {
+            assert(cs == interp->st.cstack_chain);
+            for(in_loop = 0, count=0; cs != interp->st.cstack_chain || in_loop == 0; cs = cs->next) {
                 /* Record tasklets to be killed.
                  * This loop body must not release the GIL.
                  */
@@ -785,10 +793,10 @@ current_main:
     if (target_ts == NULL || target_ts == cts) {
         PyCStackObject *cs;
 
-        if (slp_cstack_chain == NULL)
+        if (interp->st.cstack_chain == NULL)
             return;
         in_loop = 0;
-        for (cs = slp_cstack_chain; cs != slp_cstack_chain || in_loop == 0; cs = cs->next) {
+        for (cs = interp->st.cstack_chain; cs != interp->st.cstack_chain || in_loop == 0; cs = cs->next) {
             /* This loop body must not release the GIL. */
             assert(cs);
             assert(cs->next);

Stackless/module/channelobject.c
@@ -374,14 +374,13 @@ static PyMemberDef channel_members[] = {
  */
 
 
-static PyObject * channel_hook = NULL;
-
 static void
-channel_callback(PyChannelObject *channel, PyTaskletObject *task, int sending, int willblock)
+channel_callback(PyObject *channel_hook, PyChannelObject *channel, PyTaskletObject *task, int sending, int willblock)
 {
     PyObject *ret;
     PyObject *type, *value, *traceback;
 
+    assert(channel_hook);
     PyErr_Fetch(&type, &value, &traceback);
     ret = PyObject_CallFunction(channel_hook, "(OOii)", channel,
                                 task, sending, willblock);
@@ -398,31 +397,35 @@ channel_callback(PyChannelObject *channel, PyTaskletObject *task, int sending, i
 }
 
 #define NOTIFY_CHANNEL(channel, task, dir, cando, res) \
-    if(channel_hook != NULL) { \
-      if (ts->st.schedlock) {  \
-    RUNTIME_ERROR("Recursive channel call due to callbacks!", res); \
-      } \
-      ts->st.schedlock = 1; \
-      channel_callback(channel, task, dir > 0, !cando); \
-      ts->st.schedlock = 0;\
-   }
+    do { \
+        PyObject * channel_hook = ts->interp->st.channel_hook; \
+        if(channel_hook != NULL) { \
+            if (ts->st.schedlock) {  \
+                RUNTIME_ERROR("Recursive channel call due to callbacks!", res); \
+            } \
+            Py_INCREF(channel_hook);  /* Own a ref while calling channel_hook! */ \
+            ts->st.schedlock = 1; \
+            channel_callback(channel_hook, channel, task, dir > 0, !cando); \
+            ts->st.schedlock = 0; \
+            Py_DECREF(channel_hook); \
+        } \
+    } while(0)
 
 
 int PyStackless_SetChannelCallback(PyObject *callable)
 {
-    PyObject * temp = channel_hook;
+    PyThreadState * ts = PyThreadState_GET();
     if(callable != NULL && !PyCallable_Check(callable))
         TYPE_ERROR("channel callback must be callable", -1);
     Py_XINCREF(callable);
-    channel_hook = callable;
-    Py_XDECREF(temp);
+    Py_XSETREF(ts->interp->st.channel_hook, callable);
     return 0;
 }
 
 PyObject *
 slp_get_channel_callback(void)
 {
-    PyObject *temp = channel_hook;
+    PyObject *temp = PyThreadState_GET()->interp->st.channel_hook;
     Py_XINCREF(temp);
     return temp;
 }

Stackless/module/scheduling.c
@@ -14,7 +14,6 @@
 static PyBombObject *free_list = NULL;
 static int numfree = 0;         /* number of bombs currently in free_list */
 #define MAXFREELIST 20          /* max value for numfree */
-static PyBombObject *mem_bomb = NULL; /* a permanent bomb to use for memory errors */
 
 static void
 bomb_dealloc(PyBombObject *bomb)
@@ -49,8 +48,8 @@ bomb_clear(PyBombObject *bomb)
     Py_CLEAR(bomb->curexc_traceback);
 }
 
-static PyBombObject *
-new_bomb(void)
+PyBombObject *
+slp_new_bomb(void)
 {
     PyBombObject *bomb;
 
@@ -77,7 +76,7 @@ static PyObject *
 bomb_new(PyTypeObject *type, PyObject *args, PyObject *kwds)
 {
     static char *kwlist[] = {"type", "value", "traceback", NULL};
-    PyBombObject *bomb = new_bomb();
+    PyBombObject *bomb = slp_new_bomb();
 
     if (bomb == NULL)
         return NULL;
@@ -112,7 +111,7 @@ slp_make_bomb(PyObject *klass, PyObject *args, char *msg)
                    " subclass as first parameter", msg);
         TYPE_ERROR(s, NULL);
     }
-    if ( (bomb = new_bomb()) == NULL)
+    if ( (bomb = slp_new_bomb()) == NULL)
         return NULL;
     Py_INCREF(klass);
     bomb->curexc_type = klass;
@@ -157,7 +156,7 @@ slp_exc_to_bomb(PyObject *exc, PyObject *val, PyObject *tb)
         return NULL;
     }
 
-    bomb = new_bomb();
+    bomb = slp_new_bomb();
     if (bomb == NULL)
         return NULL;
 
@@ -174,12 +173,15 @@ PyObject *
 slp_curexc_to_bomb(void)
 {
     PyBombObject *bomb;
-    assert(mem_bomb != NULL);  /* assert, that the bomb-type was initialized */
+
+    /* assert, that the bomb-type was initialized */
+    assert(PyType_HasFeature(&PyBomb_Type, Py_TPFLAGS_READY));
     if (PyErr_ExceptionMatches(PyExc_MemoryError)) {
-        bomb = mem_bomb;
+        bomb = PyThreadState_GET()->interp->st.mem_bomb;
+        assert(bomb != NULL);
         Py_INCREF(bomb);
     } else
-        bomb = new_bomb();
+        bomb = slp_new_bomb();
     if (bomb != NULL)
         PyErr_Fetch(&bomb->curexc_type, &bomb->curexc_value,
                     &bomb->curexc_traceback);
@@ -309,11 +311,7 @@ PyTypeObject PyBomb_Type = {
 int
 slp_init_bombtype(void)
 {
-    if (PyType_Ready(&PyBomb_Type))
-        return -1;
-    /* allocate the permanent bomb to use for mem errors */
-    mem_bomb = new_bomb();
-    return mem_bomb ? 0 : -1;
+    return PyType_Ready(&PyBomb_Type);
 }
 
 /*******************************************************************
@@ -347,8 +345,6 @@ slp_init_bombtype(void)
 
  ********************************************************************/
 
-slp_schedule_hook_func *_slp_schedule_fasthook;
-PyObject *_slp_schedule_hook;
 
 typedef struct {
     intptr_t magic1;
@@ -437,7 +433,12 @@ slp_get_saved_tstate(PyTaskletObject *task) {
 int
 slp_schedule_callback(PyTaskletObject *prev, PyTaskletObject *next)
 {
+    PyThreadState * ts = PyThreadState_GET();
+    PyObject *schedule_hook = ts->interp->st.schedule_hook;
     PyObject *args;
+
+    if (schedule_hook == NULL)
+        return 0;
 
     if (prev == NULL) prev = (PyTaskletObject *)Py_None;
     if (next == NULL) next = (PyTaskletObject *)Py_None;
@@ -446,7 +447,8 @@ slp_schedule_callback(PyTaskletObject *prev, PyTaskletObject *next)
         PyObject *type, *value, *traceback, *ret;
 
         PyErr_Fetch(&type, &value, &traceback);
-        ret = PyObject_Call(_slp_schedule_hook, args, NULL);
+        Py_INCREF(schedule_hook);
+        ret = PyObject_Call(schedule_hook, args, NULL);
         if (ret != NULL)
             PyErr_Restore(type, value, traceback);
         else {
@@ -456,6 +458,7 @@ slp_schedule_callback(PyTaskletObject *prev, PyTaskletObject *next)
         }
         Py_XDECREF(ret);
         Py_DECREF(args);
+        Py_DECREF(schedule_hook);
         return ret ? 0 : -1;
     }
     else
@@ -463,11 +466,12 @@ slp_schedule_callback(PyTaskletObject *prev, PyTaskletObject *next)
 }
 
 static void
-slp_call_schedule_fasthook(PyThreadState *ts, PyTaskletObject *prev, PyTaskletObject *next)
+slp_call_schedule_fasthook(slp_schedule_hook_func * hook, PyThreadState *ts, PyTaskletObject *prev, PyTaskletObject *next)
 {
     int ret;
     PyObject *tmp;
     PyTaskletObject * old_current;
+    assert(hook);
     if (ts->st.schedlock) {
         Py_FatalError("Recursive scheduler call due to callbacks!");
         return;
@@ -482,7 +486,7 @@ slp_call_schedule_fasthook(PyThreadState *ts, PyTaskletObject *prev, PyTaskletOb
     old_current = ts->st.current;
     if (prev)
         ts->st.current = prev;
-    ret = _slp_schedule_fasthook(prev, next);
+    ret = hook(prev, next);
     ts->st.current = old_current;
     ts->st.schedlock = 0;
     if (ret) {
@@ -501,10 +505,13 @@ slp_call_schedule_fasthook(PyThreadState *ts, PyTaskletObject *prev, PyTaskletOb
     ts->st.del_post_switch = tmp;
 }
 
-#define NOTIFY_SCHEDULE(prev, next, errflag) \
-    if (_slp_schedule_fasthook != NULL) { \
-        slp_call_schedule_fasthook(ts, prev, next); \
-    }
+#define NOTIFY_SCHEDULE(ts, prev, next, errflag) \
+    do { \
+        slp_schedule_hook_func * hook = (ts)->interp->st.schedule_fasthook; \
+        if (hook != NULL) { \
+            slp_call_schedule_fasthook(hook, ts, prev, next); \
+        } \
+    } while(0)
 
 static void
 kill_wrap_bad_guy(PyTaskletObject *prev, PyTaskletObject *bad_guy)
@@ -836,7 +843,7 @@ schedule_task_block(PyObject **result, PyTaskletObject *prev, int stackless, int
 
 cantblock:
     /* cannot block */
-    if (revive_main || (ts == slp_initial_tstate && wakeup->next == NULL)) {
+    if (revive_main || (ts == ts->interp->st.initial_tstate && wakeup->next == NULL)) {
         /* emulate old revive_main behavior:
          * passing a value only if it is an exception
          */
@@ -1038,7 +1045,7 @@ slp_schedule_task_prepared(PyThreadState *ts, PyObject **result, PyTaskletObject
         return 0;
     }
 
-    NOTIFY_SCHEDULE(prev, next, -1);
+    NOTIFY_SCHEDULE(ts, prev, next, -1);
 
     if (!(ts->st.runflags & PY_WATCHDOG_TOTALTIMEOUT))
         ts->st.tick_watermark = ts->st.tick_counter + ts->st.interval; /* reset timeslice */
@@ -1058,7 +1065,7 @@ slp_schedule_task_prepared(PyThreadState *ts, PyObject **result, PyTaskletObject
         prev->cstate = ts->st.initial_stub;
         Py_INCREF(prev->cstate);
     }
-    if (ts != slp_initial_tstate) {
+    if (ts != ts->interp->st.initial_tstate) {
         /* ensure to get all tasklets into the other thread's chain */
         if (slp_ensure_linkage(prev) || slp_ensure_linkage(next))
             return -1;
@@ -1259,7 +1266,7 @@ slp_initialize_main_and_current(void)
     assert(task->exc_info == &task->exc_state);
     SLP_EXCHANGE_EXCINFO(ts, task);
 
-    NOTIFY_SCHEDULE(NULL, task, -1);
+    NOTIFY_SCHEDULE(ts, NULL, task, -1);
 
     return 0;
 }
@@ -1388,7 +1395,7 @@ slp_tasklet_end(PyObject *retval)
         int handled = 0;
         if (PyErr_ExceptionMatches(PyExc_SystemExit)) {
             /* but if it is truly a SystemExit on the main thread, we want the exit! */
-            if (ts == slp_initial_tstate && !PyErr_ExceptionMatches(PyExc_TaskletExit)) {
+            if (ts == ts->interp->st.initial_tstate && !PyErr_ExceptionMatches(PyExc_TaskletExit)) {
                 PyStackless_HandleSystemExit();
                 handled = 1; /* handler returned, it wants us to silence it */
             } else if (!ismain) {

Stackless/module/stacklessmodule.c
@@ -133,9 +133,6 @@ atomic_new(PyTypeObject *type, PyObject *args, PyObject *kwds)
 
  ******************************************************/
 
-PyThreadState *slp_initial_tstate = NULL;
-
-static void *slp_error_handler = NULL;
 
 int
 slp_pickle_with_tracing_state()
@@ -787,46 +784,53 @@ static PyObject *
 set_error_handler(PyObject *self, PyObject *args)
 {
     PyObject *old, *handler = NULL;
+    PyThreadState * ts = PyThreadState_GET();
     if (!PyArg_ParseTuple(args, "|O:set_error_handler", &handler))
         return NULL;
-    old = slp_error_handler ? slp_error_handler : Py_None;
+    old = ts->interp->st.error_handler;
+    if (old == NULL)
+        old = Py_None;
     Py_INCREF(old);
     if (handler == Py_None)
         handler = NULL;
     Py_XINCREF(handler);
-    Py_CLEAR(slp_error_handler);
-    slp_error_handler = handler;
+    Py_XSETREF(ts->interp->st.error_handler, handler);
     return old;
 }
 
 int
 PyStackless_CallErrorHandler(void)
 {
-    assert(PyErr_Occurred());
-    if (slp_error_handler != NULL) {
-        PyObject *exc, *val, *tb;
-        PyObject *result;
-        PyErr_Fetch(&exc, &val, &tb); /* we own the refs in exc, val and tb */
-        assert(exc != NULL);
-        if (!val) {
-            Py_INCREF(Py_None);
-            val = Py_None;
-        }
-        if (!tb) {
-            Py_INCREF(Py_None);
-            tb = Py_None;
-        }
-        result = PyObject_CallFunction(slp_error_handler, "OOO", exc, val, tb);
-        Py_DECREF(exc);
-        Py_DECREF(val);
-        Py_DECREF(tb);
-        Py_XDECREF(result);
-        if (!result)
-            return -1; /* an error in the handler! */
-        return 0;
-    } else
-        return -1; /* just raise the current exception */
+    PyThreadState * ts = PyThreadState_GET();
+    PyObject * error_handler = ts->interp->st.error_handler;
+    int res = -1;  /* just raise the current exception */
+    PyObject *exc, *val, *tb;
+    PyObject *result;
 
+    assert(PyErr_Occurred());
+    if (error_handler == NULL)
+        return res;
+
+    Py_INCREF(error_handler);  /* Own a ref while calling the handler! */
+    PyErr_Fetch(&exc, &val, &tb); /* we own the refs in exc, val and tb */
+    assert(exc != NULL);
+    if (!val) {
+        Py_INCREF(Py_None);
+        val = Py_None;
+    }
+    if (!tb) {
+        Py_INCREF(Py_None);
+        tb = Py_None;
+    }
+    result = PyObject_CallFunction(error_handler, "OOO", exc, val, tb);
+    if (result)
+        res = 0;
+    Py_DECREF(error_handler);
+    Py_DECREF(exc);
+    Py_DECREF(val);
+    Py_DECREF(tb);
+    Py_XDECREF(result);
+    return res;
 }
 
 /******************************************************
@@ -1330,17 +1334,18 @@ PyStackless_CallCMethod_Main(PyMethodDef *meth, PyObject *self, char *format, ..
 
 void PyStackless_SetScheduleFastcallback(slp_schedule_hook_func func)
 {
-    _slp_schedule_fasthook = func;
+    PyThreadState_GET()->interp->st.schedule_fasthook = func;
 }
 
 int PyStackless_SetScheduleCallback(PyObject *callable)
 {
-    PyObject * temp = _slp_schedule_hook;
+    PyThreadState * ts = PyThreadState_GET();
+    PyObject * temp = ts->interp->st.schedule_hook;
     if(callable != NULL && !PyCallable_Check(callable))
         TYPE_ERROR("schedule callback must be callable", -1);
     Py_XINCREF(callable);
-    _slp_schedule_hook = callable;
-    if (callable!=NULL)
+    ts->interp->st.schedule_hook = callable;
+    if (callable != NULL)
         PyStackless_SetScheduleFastcallback(slp_schedule_callback);
     else
         PyStackless_SetScheduleFastcallback(NULL);
@@ -1362,7 +1367,8 @@ Pass None to switch monitoring off again.");
 static PyObject *
 set_schedule_callback(PyObject *self, PyObject *arg)
 {
-    PyObject * old = _slp_schedule_hook;
+    PyThreadState * ts = PyThreadState_GET();
+    PyObject * old = ts->interp->st.schedule_hook;
     if (NULL == old)
         old = Py_None;
     Py_INCREF(old);
@@ -1382,7 +1388,8 @@ This function returns None, if no callback is installed.");
 static PyObject *
 get_schedule_callback(PyObject *self)
 {
-    PyObject *temp = _slp_schedule_hook;
+    PyThreadState * ts = PyThreadState_GET();
+    PyObject *temp = ts->interp->st.schedule_hook;
     if (NULL == temp)
         temp = Py_None;
     Py_INCREF(temp);
@@ -1594,11 +1601,13 @@ PyDoc_STRVAR(slpmodule_getdebug__doc__,
 static PyObject *
 slpmodule_getuncollectables(PyObject *self)
 {
+    PyThreadState * ts = PyThreadState_GET();
     PyObject *lis = PyList_New(0);
-    PyCStackObject *cst = slp_cstack_chain;
+    PyCStackObject *cst, *cst_first = ts->interp->st.cstack_chain;
 
     if (lis == NULL)
         return NULL;
+    cst = cst_first;
     do {
         if (cst->task != NULL) {
             if (PyList_Append(lis, (PyObject *) cst->task)) {
@@ -1607,7 +1616,7 @@ slpmodule_getuncollectables(PyObject *self)
             }
         }
         cst = cst->next;
-    } while (cst != slp_cstack_chain);
+    } while (cst != cst_first);
     return lis;
 }
 
@@ -1811,7 +1820,8 @@ int
 _PyStackless_InitTypes(void)
 {
     /* record the thread state for thread support */
-    slp_initial_tstate = PyThreadState_GET();
+    PyThreadState * ts = PyThreadState_GET();
+    ts->interp->st.initial_tstate = ts;
 
     if (0
         || slp_init_bombtype()

Stackless/module/taskletobject.c
@@ -453,7 +453,7 @@ tasklet_new(PyTypeObject *type, PyObject *args, PyObject *kwds)
     t->cstate = ts->st.initial_stub;
     t->def_globals = PyEval_GetGlobals();
     Py_XINCREF(t->def_globals);
-    if (ts != slp_initial_tstate) {
+    if (ts != ts->interp->st.initial_tstate) {
         /* make sure to kill tasklets with their thread */
         if (slp_ensure_linkage(t)) {
             Py_DECREF(t);
@@ -1155,7 +1155,7 @@ bind_tasklet_to_frame(PyTaskletObject *task, PyFrameObject *frame)
         task->cstate = ts->st.initial_stub;
         Py_INCREF(task->cstate);
         Py_DECREF(hold);
-        if (ts != slp_initial_tstate)
+        if (ts != ts->interp->st.initial_tstate)
             if (slp_ensure_linkage(task))
                 return -1;
     }

Stackless/unittests/test_shutdown.py
@@ -478,7 +478,7 @@ class TestInterpreterShutdown(unittest.TestCase):
         script = __file__
         if script.endswith("pyc") or script.endswith("pyo"):
             script = script[:-1]
-        args = [sys.executable, "-S", "-E", script, "IST"]
+        args = [sys.executable, "-s", "-S", "-E", script, "IST"]
         args.append("--running")  # always needed. Killing blocked threads is not possible
         if is_hard:
             args.append("--hard")

commit a05f33004e35504605d2e6936a30d9a73bc65488

Stackless issue #152: fix clang warnings caused by commit 0727a8ec807.
The commit used a C11 feature. Unfortunately only clang emits a warning.

Stackless/core/stackless_tstate.h
@@ -13,20 +13,23 @@
 #endif
 #endif
 
-typedef struct _cstack PyCStackObject;
-typedef struct _bomb PyBombObject;
-typedef struct _tasklet PyTaskletObject;
-typedef int (slp_schedule_hook_func) (PyTaskletObject *from, PyTaskletObject *to);
-typedef struct _ts PyThreadState;
+/* forward declarations */
+struct _cstack;
+struct _bomb;
+struct _tasklet;
+struct _ts;
+
+typedef int (slp_schedule_hook_func) (struct _tasklet *from, struct _tasklet *to);
+
 typedef struct {
-    PyCStackObject * cstack_chain;              /* the chain of all C-stacks of this interpreter. This is an uncounted/borrowed ref. */
+    struct _cstack * cstack_chain;              /* the chain of all C-stacks of this interpreter. This is an uncounted/borrowed ref. */
     PyObject * reduce_frame_func;               /* a function used to pickle frames */
     PyObject * error_handler;                   /* the Stackless error handler */
     PyObject * channel_hook;                    /* the channel callback function */
-    PyBombObject * mem_bomb;                    /* a permanent bomb to use for memory errors */
+    struct _bomb * mem_bomb;                    /* a permanent bomb to use for memory errors */
     PyObject * schedule_hook;                   /* the schedule callback function */
     slp_schedule_hook_func * schedule_fasthook; /* the fast C-only schedule_hook */
-    PyThreadState * initial_tstate;             /* recording the main thread state */
+    struct _ts * initial_tstate;                /* recording the main thread state */
     uint8_t enable_softswitch;                  /* the flag which decides whether we try to use soft switching */
     uint8_t pickleflags;                        /* flags for pickling / unpickling */
 } PyStacklessInterpreterState;
@@ -152,7 +155,6 @@ typedef struct _sts {
    we use Py_CLEAR, since it clears the pointer before deallocating.
  */
 
-struct _ts; /* Forward */
 
 void slp_kill_tasks_with_stacks(struct _ts *tstate);
 