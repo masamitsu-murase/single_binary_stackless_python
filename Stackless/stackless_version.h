ff1296d801773fe2b19ada177f4a3969eb5ce770
Anselm Kruis <Anselm.Kruis@atos.net>
2018-11-20 14:55:50 +0100

    Stackless issue #149: Bump Stackless version to "3.7"

--- Stackless/stackless_version.h
+++ Stackless/stackless_version.h
@@ -1,7 +1,6 @@
 /*
  * Stackless version string
- * created at Tue May 16 13:27:25 2006 by mkversion.py
  */
 
 /* keep this entry up-to-date */
-#define STACKLESS_VERSION "3.1b3 060516"
+#define STACKLESS_VERSION "3.7"
