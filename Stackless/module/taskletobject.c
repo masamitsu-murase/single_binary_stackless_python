masamitsu-murase/single_binary_stackless_python

commit 245a802f7aa5b5e211d3dfca847dbd87f49a4b99

Stackless issue #149: Move exc state from thread to tasklet
bpo-25612 (#1773) moves the exception state information from frame
object to coroutine (generator/thread) object where it belongs.
As a consequence Stackless moves the exception state information for
non-current tasklets from thread-state to the tasklet. This changes the
pickle format of frame, tasklet and generator objects.
The commit adds three test cases.

Stackless/module/taskletobject.c
@@ -160,6 +160,9 @@ tasklet_traverse(PyTaskletObject *t, visitproc visit, void *arg)
     Py_VISIT(t->f.frame);
     Py_VISIT(t->tempval);
     Py_VISIT(t->cstate);
+    Py_VISIT(t->exc_state.exc_type);
+    Py_VISIT(t->exc_state.exc_value);
+    Py_VISIT(t->exc_state.exc_traceback);
     return 0;
 }
 
@@ -180,6 +183,17 @@ tasklet_clear(PyTaskletObject *t)
     if (t->cstate != NULL && t->cstate->task == t)
         t->cstate->task = NULL;
     Py_CLEAR(t->cstate);
+
+    Py_CLEAR(t->exc_state.exc_type);
+    Py_CLEAR(t->exc_state.exc_value);
+    Py_CLEAR(t->exc_state.exc_traceback);
+
+    /* The stack of exception states should contain just this tasklet. */
+    assert(t->exc_info->previous_item == NULL);
+    if (Py_VerboseFlag && t->exc_info != &t->exc_state) {
+        fprintf(stderr,
+          "PyTaskletObject_Clear: warning: tasklet still has a generator\n");
+    }
 }
 
 /*
@@ -217,6 +231,20 @@ kill_finally (PyObject *ob)
 
 
 /* destructing a tasklet without destroying it */
+static inline void
+exc_state_clear(_PyErr_StackItem *exc_state)
+{
+    PyObject *t, *v, *tb;
+    t = exc_state->exc_type;
+    v = exc_state->exc_value;
+    tb = exc_state->exc_traceback;
+    exc_state->exc_type = NULL;
+    exc_state->exc_value = NULL;
+    exc_state->exc_traceback = NULL;
+    Py_XDECREF(t);
+    Py_XDECREF(v);
+    Py_XDECREF(tb);
+}
 
 static void
 tasklet_dealloc(PyTaskletObject *t)
@@ -252,6 +280,9 @@ tasklet_dealloc(PyTaskletObject *t)
     }
     Py_DECREF(t->tempval);
     Py_XDECREF(t->def_globals);
+    assert(t->exc_state.previous_item == NULL);
+    assert(t->exc_info  == &t->exc_state);
+    exc_state_clear(&t->exc_state);
     Py_TYPE(t)->tp_free((PyObject*)t);
 }
 
@@ -390,7 +421,7 @@ tasklet_bind(PyObject *self, PyObject *args, PyObject *kwargs)
     return self;
 }
 
-#define TASKLET_TUPLEFMT "iOiO"
+#define TASKLET_TUPLEFMT "iOiOOOOO"
 
 static PyObject *
 tasklet_new(PyTypeObject *type, PyObject *args, PyObject *kwds)
@@ -409,6 +440,8 @@ tasklet_new(PyTypeObject *type, PyObject *args, PyObject *kwds)
     if (t == NULL)
         return NULL;
     memset(&t->flags, 0, sizeof(t->flags));
+    memset(&t->exc_state, 0, sizeof(t->exc_state));
+    t->exc_info = &t->exc_state;
     t->recursion_depth = 0;
     t->next = NULL;
     t->prev = NULL;
@@ -472,6 +505,7 @@ tasklet_reduce(PyTaskletObject * t)
     PyObject *tup = NULL, *lis = NULL;
     PyFrameObject *f;
     PyThreadState *ts = t->cstate->tstate;
+    PyObject *exc_type, *exc_value, *exc_traceback, *exc_info;
 
     if (ts && t == ts->st.current)
         RUNTIME_ERROR("You cannot __reduce__ the tasklet which is"
@@ -500,13 +534,55 @@ tasklet_reduce(PyTaskletObject * t)
     }
     if (PyList_Reverse(lis)) goto err_exit;
     assert(t->cstate != NULL);
+
+    if (t->exc_state.previous_item != NULL) {
+        PyErr_SetString(PyExc_SystemError, "unexpected previous _PyErr_StackItem in tasklet");
+        goto err_exit;
+    }
+
+    assert(!ts || t->exc_info != &ts->exc_state);
+    /* Because of the test a few lines above, it is guaranteed that t is not the current tasklet.
+     * Therefore we can simplify the line
+     *
+     * exc_info = slp_get_obj_for_exc_state(ts && ts->st.current == t ? ts->exc_info : t->exc_info, ts)
+     *
+     * to
+     */
+    assert(!(ts && ts->st.current == t));
+    exc_info = slp_get_obj_for_exc_state(t->exc_info);
+    if (exc_info == NULL)
+        goto err_exit;
+    assert(exc_info != Py_None);
+    if (exc_info == (PyObject *)t) {
+        Py_INCREF(Py_None);
+        Py_SETREF(exc_info, Py_None);
+    }
+    assert(!PyTasklet_Check(exc_info)); /* must be a generator, coro, asynccoro, ... */
+
+    exc_type = t->exc_state.exc_type;
+    exc_value = t->exc_state.exc_value;
+    exc_traceback = t->exc_state.exc_traceback;
+    if (exc_type == NULL) exc_type = Py_None;
+    if (exc_value == NULL) exc_value = Py_None;
+    if (exc_traceback == NULL) exc_traceback = Py_None;
+    Py_INCREF(exc_type);
+    Py_INCREF(exc_value);
+    Py_INCREF(exc_traceback);
     tup = Py_BuildValue("(O()(" TASKLET_TUPLEFMT "))",
                         Py_TYPE(t),
                         tasklet_flags_as_integer(t->flags),
                         t->tempval,
                         t->cstate->nesting_level,
-                        lis
+                        lis,
+                        exc_type,
+                        exc_value,
+                        exc_traceback,
+                        exc_info
                         );
+    Py_DECREF(exc_info);
+    Py_DECREF(exc_type);
+    Py_DECREF(exc_value);
+    Py_DECREF(exc_traceback);
 err_exit:
     Py_XDECREF(lis);
     return tup;
@@ -527,15 +603,22 @@ tasklet_setstate(PyObject *self, PyObject *args)
     PyTaskletObject *t = (PyTaskletObject *) self;
     PyObject *tempval, *lis;
     int flags, nesting_level;
+    PyObject *exc_type, *exc_value, *exc_traceback;
+    PyObject *old_type, *old_value, *old_traceback;
+    PyObject *exc_info_obj;
     PyFrameObject *f;
     Py_ssize_t i, nframes;
     int j;
 
-    if (!PyArg_ParseTuple(args, "iOiO!:tasklet",
+    if (!PyArg_ParseTuple(args, "iOiO!OOOO:tasklet",
                           &flags,
                           &tempval,
                           &nesting_level,
-                          &PyList_Type, &lis))
+                          &PyList_Type, &lis,
+                          &exc_type,
+                          &exc_value,
+                          &exc_traceback,
+                          &exc_info_obj))
         return NULL;
 
     nframes = PyList_GET_SIZE(lis);
@@ -597,7 +680,52 @@ tasklet_setstate(PyObject *self, PyObject *args)
             ++t->recursion_depth;
         }
     }
+
+    old_type = t->exc_state.exc_type;
+    old_value = t->exc_state.exc_value;
+    old_traceback = t->exc_state.exc_traceback;
+    if (exc_type != Py_None) {
+        Py_INCREF(exc_type);
+        t->exc_state.exc_type = exc_type;
+    } else
+        t->exc_state.exc_type = NULL;
+    if (exc_value != Py_None) {
+        Py_INCREF(exc_value);
+        t->exc_state.exc_value = exc_value;
+    } else
+        t->exc_state.exc_value = NULL;
+    if (exc_traceback != Py_None) {
+        Py_INCREF(exc_traceback);
+        t->exc_state.exc_traceback = exc_traceback;
+    } else
+        t->exc_state.exc_value = NULL;
+    assert(t->exc_state.previous_item == NULL);
+
+    /* t must not be current, otherwise we would have to assign to ts->exc_info_obj */
+    assert(t != PyThreadState_GET()->st.current);
+    if (exc_info_obj == Py_None) {
+        t->exc_info = &t->exc_state;
+    } else {
+        /* Check the preconditions for the next assignment.
+         *
+         * The cast in the assignment is OK, because all possible concrete types of exc_info_obj
+         * have the exec_state member at the same offset.
+         */
+        assert(PyGen_Check(exc_info_obj) ||
+                PyObject_TypeCheck(exc_info_obj, &PyCoro_Type) ||
+                PyObject_TypeCheck(exc_info_obj, &PyAsyncGen_Type));
+        Py_BUILD_ASSERT(offsetof(PyGenObject, gi_exc_state) == offsetof(PyCoroObject, cr_exc_state));
+        Py_BUILD_ASSERT(offsetof(PyGenObject, gi_exc_state) == offsetof(PyAsyncGenObject, ag_exc_state));
+        /* Make sure, that *exc_info_obj stays alive after Py_DECREF(args).
+         */
+        assert(Py_REFCNT(exc_info_obj) > 1);
+        t->exc_info = &(((PyGenObject *)exc_info_obj)->gi_exc_state);
+    }
+
     Py_INCREF(self);
+    Py_XDECREF(old_type);
+    Py_XDECREF(old_value);
+    Py_XDECREF(old_traceback);
     return self;
 }
 

Include/frameobject.h
@@ -52,7 +52,7 @@ typedef struct _frame {
     char f_executing;           /* whether the frame is still executing */
     PyTryBlock f_blockstack[CO_MAXBLOCKS]; /* for try and loop blocks */
 #ifdef STACKLESS
-    PyCodeObject *f_code;	    /* code segment */
+    PyCodeObject *f_code;           /* code segment */
 #endif
     PyObject *f_localsplus[1];  /* locals+stack, dynamically sized */
 } PyFrameObject;

Objects/frameobject.c
@@ -363,14 +363,14 @@ frame_settrace(PyFrameObject *f, PyObject* v, void *closure)
 static PyObject *
 frame_getback(PyFrameObject *f, void *nope)
 {
-	PyFrameObject *fb = f->f_back;
-	PyObject *ret;
+        PyFrameObject *fb = f->f_back;
+        PyObject *ret;
     /* move over cframe objects but halt at NULL or None */
-	while (fb != NULL && (PyObject*)fb != Py_None && !PyFrame_Check(fb))
-		fb = fb->f_back;
-	ret = (PyObject *) fb;
-	if (ret == NULL)
-		ret = Py_None;
+        while (fb != NULL && (PyObject*)fb != Py_None && !PyFrame_Check(fb))
+                fb = fb->f_back;
+        ret = (PyObject *) fb;
+        if (ret == NULL)
+                ret = Py_None;
     Py_INCREF(ret);
     return ret;
 }
@@ -380,7 +380,7 @@ frame_getback(PyFrameObject *f, void *nope)
 
 static PyGetSetDef frame_getsetlist[] = {
 #ifdef STACKLESS
-    {"f_back",	(getter)frame_getback, NULL, NULL},
+    {"f_back",  (getter)frame_getback, NULL, NULL},
 #endif
     {"f_locals",        (getter)frame_getlocals, NULL, NULL},
     {"f_lineno",        (getter)frame_getlineno,

Objects/genobject.c
@@ -274,6 +274,9 @@ gen_send_ex(PyGenObject *gen, PyObject *arg, int exc, int closing)
     f->f_back->f_back = stopframe;
 
     gen->gi_running = 1;
+    assert(gen->gi_exc_state.previous_item == NULL);
+    gen->gi_exc_state.previous_item = tstate->exc_info;
+    tstate->exc_info = &gen->gi_exc_state;
 
     f->f_execute = PyEval_EvalFrameEx_slp;
 
@@ -321,6 +324,10 @@ gen_iternext_callback(PyFrameObject *f, int exc, PyObject *result)
     /* Check, that this cframe belongs to gen */
     assert(f->f_back == (PyFrameObject *)cf);
 
+    assert(gen->gi_exc_state.previous_item != NULL);
+    ts->exc_info = gen->gi_exc_state.previous_item;
+    gen->gi_exc_state.previous_item = NULL;
+    gen->gi_running = 0;
 #endif
     /* If the generator just returned (as opposed to yielding), signal
      * that the generator is exhausted. */
@@ -405,8 +412,6 @@ gen_iternext_callback(PyFrameObject *f, int exc, PyObject *result)
     }
 
 #ifdef STACKLESS
-    gen->gi_running = 0;
-
     /* Don't keep the reference to f_back any longer than necessary.  It
      * may keep a chain of frames alive or it could create a reference
      * cycle. */

Stackless/core/stackless_impl.h
@@ -402,7 +402,6 @@ PyObject * slp_eval_frame_iter(struct _frame *f,  int throwflag, PyObject *retva
 PyObject * slp_eval_frame_setup_with(struct _frame *f,  int throwflag, PyObject *retval);
 PyObject * slp_eval_frame_with_cleanup(struct _frame *f,  int throwflag, PyObject *retval);
 /* other eval_frame functions from module/scheduling.c */
-PyObject * slp_restore_exception(PyFrameObject *f, int exc, PyObject *retval);
 PyObject * slp_restore_tracing(PyFrameObject *f, int exc, PyObject *retval);
 /* other eval_frame functions from Objects/typeobject.c */
 PyObject * slp_tp_init_callback(PyFrameObject *f, int exc, PyObject *retval);
@@ -748,6 +747,7 @@ PyObject * slp_curexc_to_bomb(void);
 PyObject * slp_nomemory_bomb(void);
 PyObject * slp_bomb_explode(PyObject *bomb);
 int slp_init_bombtype(void);
+PyObject * slp_get_obj_for_exc_state(_PyErr_StackItem *exc_info);
 
 /* handy abbrevations */
 

Stackless/core/stackless_structs.h
@@ -102,10 +102,6 @@ typedef struct _tasklet_tstate {
     Py_tracefunc c_tracefunc;
     PyObject *c_profileobj;
     PyObject *c_traceobj;
-
-    PyObject *exc_type;
-    PyObject *exc_value;
-    PyObject *exc_traceback;
 } PyTaskletTStateStruc;
 
 typedef struct _tasklet {
@@ -117,10 +113,17 @@ typedef struct _tasklet {
         struct _cframe *cframe;
     } f;
     PyObject *tempval;
+    struct _cstack *cstate;
+    /* Pointer to the top of the stack of the exceptions currently
+     * being handled */
+    _PyErr_StackItem *exc_info;
+    /* The exception currently being handled, if no coroutines/generators
+     * are present. Always last element on the stack referred to be exc_info.
+     */
+    _PyErr_StackItem exc_state;
     /* bits stuff */
     struct _tasklet_flags flags;
     int recursion_depth;
-    struct _cstack *cstate;
     PyObject *def_globals;
     PyObject *tsk_weakreflist;
 } PyTaskletObject;

Stackless/module/scheduling.c
@@ -357,12 +357,12 @@ typedef struct {
 } saved_tstat_with_magic_t;
 
 /* not a valid ptr and not a common integer */
-#define SAVED_TSTATE_MAGIC1 (((intptr_t)transfer_with_exc)+1)
-#define SAVED_TSTATE_MAGIC2 (-1*((intptr_t)transfer_with_exc))
+#define SAVED_TSTATE_MAGIC1 (((intptr_t)transfer_with_tracing)+1)
+#define SAVED_TSTATE_MAGIC2 (-1*((intptr_t)transfer_with_tracing))
 saved_tstat_with_magic_t * _dont_optimise_away_saved_tstat_with_magic;
 
 static int
-transfer_with_exc(PyCStackObject **cstprev, PyCStackObject *cst, PyTaskletObject *prev)
+transfer_with_tracing(PyCStackObject **cstprev, PyCStackObject *cst, PyTaskletObject *prev)
 {
     PyThreadState *ts = PyThreadState_GET();
     int ret;
@@ -384,24 +384,15 @@ transfer_with_exc(PyCStackObject **cstprev, PyCStackObject *cst, PyTaskletObject
     Py_XINCREF(sm.s.c_profileobj);
     Py_XINCREF(sm.s.c_traceobj);
 
-    sm.s.exc_type = ts->exc_type;
-    sm.s.exc_value = ts->exc_value;
-    sm.s.exc_traceback = ts->exc_traceback;
-
     PyEval_SetTrace(NULL, NULL);
     PyEval_SetProfile(NULL, NULL);
     ts->tracing = 0;
-    ts->exc_type = ts->exc_value = ts->exc_traceback = NULL;
 
     ret = slp_transfer(cstprev, cst, prev);
 
     ts->tracing = sm.s.tracing;
     PyEval_SetTrace(sm.s.c_tracefunc, sm.s.c_traceobj);
     PyEval_SetProfile(sm.s.c_profilefunc, sm.s.c_profileobj);
-
-    ts->exc_type = sm.s.exc_type;
-    ts->exc_value = sm.s.exc_value;
-    ts->exc_traceback = sm.s.exc_traceback;
 
     sm.magic1 = 0;
     sm.magic2 = 0;
@@ -548,37 +539,6 @@ kill_wrap_bad_guy(PyTaskletObject *prev, PyTaskletObject *bad_guy)
 /* slp_schedule_task is moved down and merged with soft switching */
 
 /* non-recursive scheduling */
-
-PyObject *
-slp_restore_exception(PyFrameObject *f, int exc, PyObject *retval)
-{
-    PyObject *tmp_type, *tmp_value, *tmp_tb;
-    PyThreadState *ts = PyThreadState_GET();
-    PyCFrameObject *cf = (PyCFrameObject *) f;
-
-    if (cf->ob1 == NULL) {
-        /* set_exc_info in ceval.c contains the same assertions */
-        assert(cf->ob2 == NULL);
-        assert(cf->ob3 == NULL);
-    }
-
-    /* Set new exception for this thread.
-     * This code is follows suit the code of set_exc_info() in ceval.c */
-    tmp_type = ts->exc_type;
-    tmp_value = ts->exc_value;
-    tmp_tb = ts->exc_traceback;
-    ts->exc_type = cf->ob1;
-    ts->exc_value = cf->ob2;
-    ts->exc_traceback = cf->ob3;
-    cf->ob1 = cf->ob2 = cf->ob3 = NULL;
-
-    Py_XDECREF(tmp_type);
-    Py_XDECREF(tmp_value);
-    Py_XDECREF(tmp_tb);
-
-    SLP_STORE_NEXT_FRAME(ts, cf->f_back);
-    return STACKLESS_PACK(ts, retval);
-}
 
 PyObject *
 slp_restore_tracing(PyFrameObject *f, int exc, PyObject *retval)
@@ -730,6 +690,49 @@ new_lock(void)
 
 #define acquire_lock(lock, flag) PyThread_acquire_lock(get_lock(lock), flag)
 #define release_lock(lock) PyThread_release_lock(get_lock(lock))
+
+/*
+ * Handling exception information during scheduling
+ * (since Stackless Python 3.7)
+ *
+ * Regular C-Python stores the exception state in the thread state,
+ * whereas Stackless stores the exception state in the tasklet object.
+ * When switching from one tasklet to another tasklet, we have to switch
+ * the exc_info-pointer in the thread state.
+ */
+
+#if 0
+#define SLP_EXCHANGE_EXCINFO(tstate, task) \
+    do { \
+        PyThreadState *ts_ = (tstate); \
+        PyTaskletObject *t_ = (task); \
+        _PyErr_StackItem *exc_info; \
+        PyObject * c = PyStackless_GetCurrent(); \
+        assert(ts_); \
+        assert(t_); \
+        exc_info = ts_->exc_info; \
+        assert(exc_info); \
+        assert(t_->exc_info); \
+        fprintf(stderr, "SLP_EXCHANGE_EXCINFO %3d current %14p,\tset task %p = %p,\ttstate %p = %p\n", __LINE__, c, t_, exc_info, ts_, t_->exc_info); \
+        Py_XDECREF(c); \
+        ts_->exc_info = t_->exc_info; \
+        t_->exc_info = exc_info; \
+    } while(0)
+#else
+#define SLP_EXCHANGE_EXCINFO(tstate, task) \
+    do { \
+        PyThreadState *ts_ = (tstate); \
+        PyTaskletObject *t_ = (task); \
+        _PyErr_StackItem *exc_info; \
+        assert(ts_); \
+        assert(t_); \
+        exc_info = ts_->exc_info; \
+        assert(exc_info); \
+        assert(t_->exc_info); \
+        ts_->exc_info = t_->exc_info; \
+        t_->exc_info = exc_info; \
+    } while(0)
+#endif
 
 static int schedule_thread_block(PyThreadState *ts)
 {
@@ -1017,7 +1020,6 @@ slp_schedule_task_prepared(PyThreadState *ts, PyObject **result, PyTaskletObject
                   int *did_switch)
 {
     PyCStackObject **cstprev;
-
     PyObject *retval;
     int (*transfer)(PyCStackObject **, PyCStackObject *, PyTaskletObject *);
     int transfer_result;
@@ -1062,44 +1064,6 @@ slp_schedule_task_prepared(PyThreadState *ts, PyObject **result, PyTaskletObject
             return -1;
     }
 
-    /* handle exception */
-    if (ts->exc_type == Py_None) {
-        Py_XDECREF(ts->exc_type);
-        ts->exc_type = NULL;
-    }
-    if (ts->exc_type != NULL) {
-        /* build a shadow frame if we are returning here */
-        assert(ts->frame != NULL);  /* XXX: Anselm, why ? */
-        /* The following code follows suit the code of sys.exc_clear() and
-         * ceval.c set_exc_info()
-         */
-        if (prev->f.frame != NULL) {
-            PyCFrameObject *f;
-            /* the following assert justifies the slp_cframe_new(..., 1) to link the frames */
-            assert(prev->f.frame == SLP_CURRENT_FRAME(ts));
-            f = slp_cframe_new(slp_restore_exception, 1);
-            if (f == NULL)
-                return -1;
-            f->ob1 = ts->exc_type;
-            f->ob2 = ts->exc_value;
-            f->ob3 = ts->exc_traceback;
-            ts->exc_type = NULL;
-            ts->exc_value = NULL;
-            ts->exc_traceback = NULL;
-            Py_SETREF(prev->f.frame, (PyFrameObject *) f);
-        } else {
-            PyObject *tmp_type, *tmp_value, *tmp_tb;
-            tmp_type = ts->exc_type;
-            tmp_value = ts->exc_value;
-            tmp_tb = ts->exc_traceback;
-            ts->exc_type = NULL;
-            ts->exc_value = NULL;
-            ts->exc_traceback = NULL;
-            Py_XDECREF(tmp_type);
-            Py_XDECREF(tmp_value);
-            Py_XDECREF(tmp_tb);
-        }
-    }
     if (ts->use_tracing || ts->tracing) {
         /* build a shadow frame if we are returning here */
         if (prev->f.frame != NULL) {
@@ -1170,8 +1134,10 @@ slp_schedule_task_prepared(PyThreadState *ts, PyObject **result, PyTaskletObject
             retval = slp_bomb_explode(retval);
     }
     /* no failure possible from here on */
+    SLP_EXCHANGE_EXCINFO(ts, prev);
     ts->recursion_depth = next->recursion_depth;
     ts->st.current = next;
+    SLP_EXCHANGE_EXCINFO(ts, next);
     if (did_switch)
         *did_switch = 1;
     *result = STACKLESS_PACK(ts, retval);
@@ -1186,23 +1152,27 @@ hard_switching:
 
     ts->st.current = next;
 
-    if (ts->exc_type == Py_None) {
-        Py_XDECREF(ts->exc_type);
-        ts->exc_type = NULL;
-    }
     ts->recursion_depth = next->recursion_depth;
     SLP_STORE_NEXT_FRAME(ts, next->f.frame);
     Py_CLEAR(next->f.frame);
 
     ++ts->st.nesting_level;
-    if (ts->exc_type != NULL || ts->use_tracing || ts->tracing)
-        transfer = transfer_with_exc;
+    if (ts->use_tracing || ts->tracing)
+        transfer = transfer_with_tracing;
     else
         transfer = slp_transfer;
 
+    SLP_EXCHANGE_EXCINFO(ts, prev);
+    SLP_EXCHANGE_EXCINFO(ts, next);
+
     transfer_result = transfer(cstprev, next->cstate, prev);
+    /* Note: If the transfer was successful from here on "prev" holds the
+     *       currently executing tasklet and "next" is the previous tasklet.
+     */
+
     --ts->st.nesting_level;
     if (transfer_result >= 0) {
+        /* Successful transfer. */
         PyFrameObject *f = SLP_CLAIM_NEXT_FRAME(ts);
 
         TASKLET_CLAIMVAL(prev, &retval);
@@ -1240,6 +1210,9 @@ hard_switching:
         return 0;
     }
     else {
+        /* Failed transfer. */
+        SLP_EXCHANGE_EXCINFO(ts, next);
+        SLP_EXCHANGE_EXCINFO(ts, prev);
         PyFrameObject *f = SLP_CLAIM_NEXT_FRAME(ts);
         Py_XDECREF(f);
         kill_wrap_bad_guy(prev, next);
@@ -1278,6 +1251,13 @@ slp_initialize_main_and_current(void)
     Py_INCREF(task);
     slp_current_insert(task);
     ts->st.current = task;
+
+    assert(task->exc_state.exc_type == NULL);
+    assert(task->exc_state.exc_value == NULL);
+    assert(task->exc_state.exc_traceback == NULL);
+    assert(task->exc_state.previous_item == NULL);
+    assert(task->exc_info == &task->exc_state);
+    SLP_EXCHANGE_EXCINFO(ts, task);
 
     NOTIFY_SCHEDULE(NULL, task, -1);
 
@@ -1368,6 +1348,9 @@ schedule_task_destruct(PyObject **retval, PyTaskletObject *prev, PyTaskletObject
         }
     } else {
         /* main is exiting */
+        assert(ts->st.main == NULL);
+        assert(ts->exc_info == &prev->exc_state);
+        SLP_EXCHANGE_EXCINFO(ts, prev);
         TASKLET_CLAIMVAL(prev, retval);
         if (PyBomb_Check(*retval))
             *retval = slp_bomb_explode(*retval);
@@ -1391,6 +1374,7 @@ slp_tasklet_end(PyObject *retval)
     PyThreadState *ts = PyThreadState_GET();
     PyTaskletObject *task = ts->st.current;
     PyTaskletObject *next;
+    _PyErr_StackItem *exc_info;
 
     int ismain = task == ts->st.main;
     int schedule_fail;
@@ -1461,14 +1445,15 @@ slp_tasklet_end(PyObject *retval)
      * of their execution.
      * The code follows suit the code of sys.exc_clear().
      */
-    if (ts->exc_type != NULL && ts->exc_type != Py_None) {
+    exc_info = _PyErr_GetTopmostException(ts);
+    if (exc_info->exc_type != NULL && exc_info->exc_type != Py_None) {
         PyObject *tmp_type, *tmp_value, *tmp_tb;
-        tmp_type = ts->exc_type;
-        tmp_value = ts->exc_value;
-        tmp_tb = ts->exc_traceback;
-        ts->exc_type = NULL;
-        ts->exc_value = NULL;
-        ts->exc_traceback = NULL;
+        tmp_type = exc_info->exc_type;
+        tmp_value = exc_info->exc_value;
+        tmp_tb = exc_info->exc_traceback;
+        exc_info->exc_type = NULL;
+        exc_info->exc_value = NULL;
+        exc_info->exc_traceback = NULL;
         Py_DECREF(tmp_type);
         Py_XDECREF(tmp_value);
         Py_XDECREF(tmp_tb);

commit cb1c4eae49c3346633421e3fe4c9c53d98ac8ff6

Stackless issue #149: Fix exc state handling during tasklet deallocation
Fix a few incorrect assertions introduced by commit 245a802f.
A test in the upcoming commit for Stackless issue #190 triggered an
assertion failure.

Stackless/module/taskletobject.c
@@ -189,12 +189,13 @@ tasklet_clear(PyTaskletObject *t)
     Py_CLEAR(t->exc_state.exc_value);
     Py_CLEAR(t->exc_state.exc_traceback);
 
-    /* The stack of exception states should contain just this tasklet. */
-    assert(t->exc_info->previous_item == NULL);
-    if (Py_VerboseFlag && t->exc_info != &t->exc_state) {
-        fprintf(stderr,
-          "PyTaskletObject_Clear: warning: tasklet still has a generator\n");
-    }
+    /* Assert that the tasklet is at the end of the chain. */
+    assert(t->exc_state.previous_item == NULL);
+    /* Unlink the exc_info chain. There is no guarantee, that
+     * the object t->exc_info points to still exists, because
+     * the order of calls to tp_clear is undefined.
+     */
+    t->exc_info = &t->exc_state;
 }
 
 /*
@@ -281,8 +282,13 @@ tasklet_dealloc(PyTaskletObject *t)
     }
     Py_DECREF(t->tempval);
     Py_XDECREF(t->def_globals);
+    /* Assert that the tasklet is at the end of the chain. */
     assert(t->exc_state.previous_item == NULL);
-    assert(t->exc_info  == &t->exc_state);
+    /* Unlink the exc_info chain. There is no guarantee, that
+     * the object t->exc_info points to still exists, because
+     * the order of calls to tp_clear is undefined.
+     */
+    t->exc_info = &t->exc_state;
     exc_state_clear(&t->exc_state);
     Py_TYPE(t)->tp_free((PyObject*)t);
 }