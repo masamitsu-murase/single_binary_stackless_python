3629160681a13eb44077d76603b223f032161a81
Anselm Kruis <anselm.kruis@atos.net>
2019-01-06 15:48:19 +0100

    Stackless issue #200: fix PyTasklet_Run_nr() and PyTasklet_Switch_nr()
    Fix a bug in the C-API functions PyTasklet_Run_nr() and
    PyTasklet_Switch_nr(). Under exotic conditions the functions could
    cause undefined behavior.

--- Stackless/module/taskletobject.c
+++ Stackless/module/taskletobject.c
@@ -936,7 +936,8 @@ impl_tasklet_run_remove(PyTaskletObject *task, int remove);
 int
 PyTasklet_Run_nr(PyTaskletObject *task)
 {
-    (_PyRuntime.st.try_stackless) = 1;
+    PyThreadState *ts = PyThreadState_GET();
+    STACKLESS_PROPOSE_ALL(ts);
     return slp_return_wrapper(impl_tasklet_run_remove(task, 0));
 }
 
@@ -1061,7 +1062,8 @@ PyTasklet_Switch_M(PyTaskletObject *task)
 int
 PyTasklet_Switch_nr(PyTaskletObject *task)
 {
-    (_PyRuntime.st.try_stackless) = 1;
+    PyThreadState *ts = PyThreadState_GET();
+    STACKLESS_PROPOSE_ALL(ts);
     return slp_return_wrapper(impl_tasklet_run_remove(task, 1));
 }
 
