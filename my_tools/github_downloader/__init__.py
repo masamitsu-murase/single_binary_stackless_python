29701b983c1e73a7566b0dc4224d54781bb7153f
Masamitsu MURASE <masamitsu.murase@gmail.com>
2019-05-14 23:37:31 +0900

    Support removing file.

--- my_tools/github_downloader/__init__.py
+++ my_tools/github_downloader/__init__.py
@@ -24,25 +24,27 @@ def extract_zipped_file_with_filter(data, output_dir, path_converter):
                     f.write(file.read(info.filename))
 
 
-def extract_zipped_file_partially(data, output_dir, directory=""):
+def extract_zipped_file_partially(data, output_dir, directory="", skip_depth=None):
     directory_split = list(filter(None, directory.split("/")))
+    if skip_depth is None:
+        skip_depth = len(directory_split)
 
     def path_converter(path):
         path_split = path.split("/")[1:]  # Skip first tag directory.
         if directory_split == path_split[:len(directory_split)]:
-            return "/".join(path_split[len(directory_split):])
+            return "/".join(path_split[skip_depth:])
         else:
             return None
 
     return extract_zipped_file_with_filter(data, output_dir, path_converter)
 
 
-def download_zip_with_filter(url, output_dir, filter_path=""):
+def download_zip_with_filter(url, output_dir, filter_path="", skip_depth=None):
     response = requests.get(url)
     if not response.ok:
         raise RuntimeError("GitHub access error: '%s': %d" % (url, response.status_code))
 
-    extract_zipped_file_partially(response.content, output_dir, filter_path)
+    extract_zipped_file_partially(response.content, output_dir, filter_path, skip_depth)
 
 
 def github_tags(github_user, github_repository):
@@ -52,21 +54,21 @@ def github_tags(github_user, github_repository):
     return json.loads(response.text)
 
 
-def download_tag_with_filter(github_user, github_repository, tag, output_dir, filter_path=""):
+def download_tag_with_filter(github_user, github_repository, tag, output_dir, filter_path="", skip_depth=None):
     tags = github_tags(github_user, github_repository)
     url = next(x["zipball_url"] for x in tags if x["name"] == tag)
-    download_zip_with_filter(url, output_dir, filter_path)
+    download_zip_with_filter(url, output_dir, filter_path, skip_depth)
 
 
-def download_latest_tag_with_filter(github_user, github_repository, output_dir, filter_path):
+def download_latest_tag_with_filter(github_user, github_repository, output_dir, filter_path="", skip_depth=None):
     tags = github_tags(github_user, github_repository)
     url = tags[0]["zipball_url"]
-    download_zip_with_filter(url, output_dir, filter_path)
+    download_zip_with_filter(url, output_dir, filter_path, skip_depth)
 
 
-def download_sha_with_filter(github_user, github_repository, sha, output_dir, filter_path=""):
+def download_sha_with_filter(github_user, github_repository, sha, output_dir, filter_path="", skip_depth=None):
     url = "https://api.github.com/repos/%s/%s/zipball/%s" % (github_user, github_repository, sha)
-    download_zip_with_filter(url, output_dir, filter_path)
+    download_zip_with_filter(url, output_dir, filter_path, skip_depth)
 
 
 if __name__ == "__main__":
