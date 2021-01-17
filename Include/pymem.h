2e4410556027f71dffbf1da69ce460854475cf8b
Masamitsu MURASE <masamitsu.murase@gmail.com>
2021-01-18 00:57:36 +0900

    Follow core code changes.
    * Include internal/pystate.h to resolve _PyRuntime definition error.
    * Follow filename changes.

--- Include/pymem.h
+++ Include/pymem.h
@@ -229,12 +229,14 @@ PyAPI_FUNC(void) PyMem_SetupDebugHooks(void);
 #endif
 
 #ifdef Py_BUILD_CORE
+#if !defined(SINGLE_BINARY_STACKLESS_PYTHON_XXLIMITED)
 /* Set the memory allocator of the specified domain to the default.
    Save the old allocator into *old_alloc if it's non-NULL.
    Return on success, or return -1 if the domain is unknown. */
 PyAPI_FUNC(int) _PyMem_SetDefaultAllocator(
     PyMemAllocatorDomain domain,
     PyMemAllocatorEx *old_alloc);
+#endif
 #endif
 
 #ifdef __cplusplus
