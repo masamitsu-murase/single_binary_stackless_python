a2ba9ae70cc4569166ce290d16638fd0f930ef63
Masamitsu MURASE <masamitsu.murase@gmail.com>
2021-01-24 00:45:18 +0900

    Update test_pyyaml.

--- PCbuild/my_test/test_pyyaml/tests/lib3/test_yaml.py
+++ PCbuild/my_test/test_pyyaml/tests/lib3/test_yaml.py
@@ -11,6 +11,8 @@ from test_emitter import *
 from test_representer import *
 from test_recursive import *
 from test_input_output import *
+from test_sort_keys import *
+from test_multi_constructor import *
 
 if __name__ == '__main__':
     import test_appliance
