3319e19020e2dc2a4790a240390258979fd80881
Masamitsu MURASE <masamitsu.murase@gmail.com>
2021-01-24 23:41:44 +0900

    Update pyreadline to suppress warning.

--- Lib/pyreadline/py3k_compat.py
+++ Lib/pyreadline/py3k_compat.py
@@ -2,10 +2,10 @@ from __future__ import print_function, unicode_literals, absolute_import
 import sys
 
 if sys.version_info[0] >= 3:
-    import collections
+    import collections.abc
     PY3 = True
     def callable(x):
-        return isinstance(x, collections.Callable)
+        return isinstance(x, collections.abc.Callable)
     
     def execfile(fname, glob, loc=None):
         loc = loc if (loc is not None) else glob
